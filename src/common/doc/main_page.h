8c98ccb2c339e6817b8deed732cdffb216ed2cf2
salinasd <salinasd@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2014-12-16 09:56:44 +0000

    doc
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/trunk@355 636b058d-ea47-450e-bf9e-a15bfbe3eedb

--- src/common/doc/main_page.h
+++ src/common/doc/main_page.h
@@ -30,7 +30,7 @@ Some packages require additional libraries :
 The procedure to install these packages according to your operating system is 
 detailled here http://doc.cgal.org/latest/Manual/installation.html
 
-The library compiles in Linux, Mac OSX and Visual Studio 2013.
+The library compiles in Linux, Mac OSX. 
 
 \section d Demos and Examples
 
