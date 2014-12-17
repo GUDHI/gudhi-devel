1bdf7e623948b6737def08bcbff067a800cf9fa0
vrouvrea <vrouvrea@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2014-12-17 14:16:05 +0000

    Version 1.1.0 - GMPXX removal from simplex_tree_from_alpha_shapes example - GMP and CGAL text modification on main page
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/trunk@375 636b058d-ea47-450e-bf9e-a15bfbe3eedb
    
    Former-commit-id: 6af168d44ff2e1a6220d1050221884f36e8fd79b

--- src/common/doc/main_page.h
+++ src/common/doc/main_page.h
@@ -10,28 +10,43 @@ state-of-the-art algorithms and data structures for computational topology.
 
 The current release of the library allows to use several data-structures for simplicial complexes :
 simplex tree, Hasse diagram or skeleton-blocker. Several  operations can then be done on top of these
-representations such a spersistent homology computation or simplification. 
+representations such a persistent homology computation or simplification.
 
 All data-structures are generic and several of their aspects (such as stored elements, policies) 
-can be parametrized via template classes.
+can be parameterized via template classes.
 
 We refer to 
 \cite gudhilibrary_ICMS14
 for a detailed description of the design of the library.
 
 
-\section Compiling
+\section compiling Compiling
 
 The library uses c++11 and requires Boost with version 1.48.0 or more recent :  http://www.boost.org/.
-The multi-field persistent homology algorithm has a dependency with GMP and some demos requires CGAL https://www.cgal.org/.
 
+The library compiles in Linux and Mac OSX.
 
-The procedure to install these libraries according to your operating system is 
-detailled here http://doc.cgal.org/latest/Manual/installation.html
+\subsection gmp GMP:
+The multi-field persistent homology algorithm requires GMP which is a free library for arbitrary-precision
+arithmetic, operating on signed integers, rational numbers, and floating point numbers
 
-The library compiles in Linux and Mac OSX. 
+The following examples require The GNU Multiple Precision Arithmetic Library (GMP) http://gmplib.org/
+and will not be built if GMP is not installed:
+  - Persistent_cohomology/rips_multifield_persistence
+  - Simplex_tree/simplex_tree_from_alpha_shapes_3
 
-\section d Demos and Examples
+Having GMP version 4.2 or higher installed is recommended. This library can be obtained from http://gmplib.org/
+
+\subsection cgal CGAL:
+CGAL is a C++ library which provides easy access to efficient and reliable geometric algorithms.
+
+The following example requires CGAL https://www.cgal.org/ and will not be built if CGAL is not installed:
+  - Simplex_tree/simplex_tree_from_alpha_shapes_3
+
+Having CGAL version 4.5 or higher installed is recommended. The procedure to install this library according to
+your operating system is detailed here http://doc.cgal.org/latest/Manual/installation.html
+
+\section demos Demos and Examples
 
 To build the library, run the following in a terminal:
 
