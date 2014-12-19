6e2343ff3fb1e2869dea5f8b6526d0e20a64be48
glisse <glisse@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2014-12-19 17:48:47 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/trunk@397 636b058d-ea47-450e-bf9e-a15bfbe3eedb
    Former-commit-id: dee74763ef7ee12bc36b6ca28f22df1d8e6e3402

--- src/Persistent_cohomology/include/gudhi/Persistent_cohomology.h
+++ src/Persistent_cohomology/include/gudhi/Persistent_cohomology.h
@@ -222,11 +222,8 @@ class Persistent_cohomology {
 
   /** \brief Initializes the Persistent_cohomology class.
    *
-   * @param[in] cpx Complex for which the persistent homology is compiuted.
+   * @param[in] cpx Complex for which the persistent homology is computed.
    cpx is a model of FilteredComplex
-   *
-   * @param[in] persistence_dim_max if true, the persistent homology for the maximal dimension in the
-   *                                complex is computed. If false, it is ignored. Default is false.
    */
   explicit Persistent_cohomology(Complex_ds& cpx)
       : cpx_(&cpx),
