d7a3c90ef611b1c104b6a4ee900b56bf39d56cf5
salinasd <salinasd@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2015-01-27 12:26:41 +0000

    clean computation of knearestneighbors
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/trunk@431 636b058d-ea47-450e-bf9e-a15bfbe3eedb
    
    Former-commit-id: e3f533ede1ffcf614008f49c23b120fe5fbda9d8

--- src/GudhUI/utils/K_nearest_builder.h
+++ src/GudhUI/utils/K_nearest_builder.h
@@ -13,51 +13,26 @@
 #include <CGAL/Euclidean_distance.h>
 #include <CGAL/Orthogonal_k_neighbor_search.h>
 #include <CGAL/Search_traits_d.h>
+#include <CGAL/Search_traits_adapter.h>
+#include <CGAL/property_map.h>
+#include <boost/iterator/zip_iterator.hpp>
 
 #include "utils/UI_utils.h"
 #include "model/Complex_typedefs.h"
 
+
+
 template<typename SkBlComplex> class K_nearest_builder{
 private:
 
-	//todo uggh no virtual delete operator in Point
-	// so do a composition asap
-	class Point_d_with_id : public Point{
-		typedef Point Base;
-	public:
-		Complex::Vertex_handle vertex_handle;
-		Point_d_with_id(int d=0) : Point(d) {}
-//		Point_d_with_id(int d, const Origin &o) : Base(d,o) {}
-
-		Point_d_with_id(int a, int b, int c = 1) :
-			Base(RT(a),RT(b),RT(c)) {}
-		Point_d_with_id(int a, int b, int c, int d) :
-			Base(RT(a),RT(b),RT(c),RT(d)) {}
-
-		template <class InputIterator>
-		Point_d_with_id (int d, InputIterator first, InputIterator last)
-		: Base (d, first, last) {}
-		template <class InputIterator>
-
-		Point_d_with_id(const Point_d_with_id &p) : Base(p) {}
-		Point_d_with_id(const Base& p) : Base(p) {}
-		Point_d_with_id(const Base& p,Complex::Vertex_handle v) : Base(p),vertex_handle(v)	{}
-
-	};
-
-	struct Kernel_with_id : public Geometry_trait{
-		typedef Point_d_with_id Point_d;
-	};
-
-
-	/**
-	 * TODO wrap vertex handle in class passed to tree
-	 */
-	typedef Kernel_with_id K;
-	typedef typename K::Point_d Point_d;
-	typedef typename CGAL::Search_traits_d<K> TreeTraits;
-	typedef typename CGAL::Orthogonal_k_neighbor_search<TreeTraits> Neighbor_search;
-	typedef typename Neighbor_search::Tree Tree;
+	typedef Geometry_trait Kernel;
+	typedef Point Point_d;
+	typedef boost::tuple<Point_d, int> Point_d_with_id;
+	typedef CGAL::Search_traits_d<Kernel> Traits_base;
+	typedef CGAL::Search_traits_adapter<Point_d_with_id, CGAL::Nth_of_tuple_property_map<0, Point_d_with_id>, Traits_base> Traits;
+	typedef CGAL::Orthogonal_k_neighbor_search<Traits> Neighbor_search;
+	typedef Neighbor_search::Tree Tree;
+	typedef Neighbor_search::Distance Distance;
 
 	SkBlComplex& complex_;
 public:
@@ -83,17 +58,16 @@ private:
 
 		std::list<Point_d_with_id> points_with_id;
 		for(auto v: complex_.vertex_range()){
-			Point_d_with_id point_v(complex_.point(v),v);
+			Point_d_with_id point_v(complex_.point(v),v.vertex);
 			points_with_id.push_back(point_v);
 		}
 
 		Tree tree(points_with_id.begin(),points_with_id.end());
 
-
 		for (auto p : complex_.vertex_range()){
 			Neighbor_search search(tree, complex_.point(p),k+1);
 			for(auto it = ++search.begin(); it != search.end(); ++it){
-				auto q = it->first.vertex_handle;
+				auto q = boost::get<1>(it->first);
 				if (p != q && complex_.contains_vertex(p) && complex_.contains_vertex(q))
 					complex_.add_edge(p,q);
 			}
