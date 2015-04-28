0b881d5dd5237e545efd6ada1010c51f1f50badd
vrouvrea <vrouvrea@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2015-04-28 11:34:43 +0000

    GudhUI compilation issue and generate version for GudhUI issue fixes
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/trunk@583 636b058d-ea47-450e-bf9e-a15bfbe3eedb
    
    Former-commit-id: beea454e849615bb9d087ccdcb0110a146a42564

--- src/GudhUI/model/Model.h
+++ src/GudhUI/model/Model.h
@@ -72,8 +72,6 @@ class Model{
 public:
 	Complex complex_;
 	typedef Complex::Vertex_handle Vertex_handle;
-	typedef Complex::CVI CVI;
-
 
 	Model():complex_(){
 	}
@@ -317,7 +315,7 @@ private:
 	void run_chomp(){
 		save_complex_in_file_for_chomp();
 		std::cout << "Call CHOMP library\n";
-		system("../src/utils/homsimpl chomp.sim");
+		system("utils/homsimpl chomp.sim");
 	}
 
 	void save_complex_in_file_for_chomp(){
