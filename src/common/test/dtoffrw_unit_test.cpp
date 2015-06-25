e1b8ee37a896ddb8d733ecd8752eb70307521f7a
vrouvrea <vrouvrea@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2015-06-25 08:25:54 +0000

    File was not renamed here
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/branches/alphashapes@643 636b058d-ea47-450e-bf9e-a15bfbe3eedb
    
    Former-commit-id: ea5a10408d8f44eb508ae68cb3ed0699dc4eba93

--- src/common/test/dtoffrw_unit_test.cpp
+++ src/common/test/dtoffrw_unit_test.cpp
@@ -43,7 +43,7 @@ typedef CGAL::Delaunay_triangulation<K> T;
 BOOST_AUTO_TEST_CASE( Delaunay_triangulation_doc_test )
 {
   // Read the OFF file (input file name given as parameter) and triangulates points
-  Gudhi::Delaunay_triangulation_off_reader<T> off_reader("../../../data/points/alphashapedoc.off");
+  Gudhi::Delaunay_triangulation_off_reader<T> off_reader("../../../data/points/alphacomplexdoc.off");
   // Check the read operation was correct
   BOOST_CHECK(off_reader.is_valid());
   
@@ -75,7 +75,7 @@ BOOST_AUTO_TEST_CASE( Delaunay_triangulation_unexisting_file_read_test )
 BOOST_AUTO_TEST_CASE( Delaunay_triangulation_unexisting_file_write_test )
 {
   // Read the OFF file (input file name given as parameter) and triangulates points
-  Gudhi::Delaunay_triangulation_off_reader<T> off_reader("../../../data/points/alphashapedoc.off");
+  Gudhi::Delaunay_triangulation_off_reader<T> off_reader("../../../data/points/alphacomplexdoc.off");
   
   // Retrieve the triangulation
   T* triangulation = off_reader.get_complex();
