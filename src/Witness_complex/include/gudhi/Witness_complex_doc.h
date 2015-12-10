6775c601c9c9f7a1f0679dbb1fecb3bd5181eb6a
skachano <skachano@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2015-12-10 09:38:37 +0000

    Changed a phrase in doc
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/branches/witness@938 636b058d-ea47-450e-bf9e-a15bfbe3eedb
    
    Former-commit-id: 407e6784210123fbd00b71efad0659f2d7c9263b

--- src/Witness_complex/include/gudhi/Witness_complex_doc.h
+++ src/Witness_complex/include/gudhi/Witness_complex_doc.h
@@ -23,8 +23,8 @@
 
    The principal class of this module is Gudhi::Witness_complex.
 
-   In both cases, the constructor for this class takes a {witness}x{closest_landmarks} table,
-   which can be constructed by two additional classes Landmark_choice_by_furthest_point and Landmark_choice_by_random_point also included in the module.
+   In both cases, the constructor for this class takes a {witness}x{closest_landmarks} table, where each row represents a witness and consists of landmarks sorted by distance to this witness.
+   This table can be constructed by two additional classes Landmark_choice_by_furthest_point and Landmark_choice_by_random_point also included in the module.
 
    *\image html "bench_Cy8.png" "Running time as function on number of landmarks" width=10cm
    *\image html "bench_sphere.png" "Running time as function on number of witnesses for |L|=300" width=10cm
