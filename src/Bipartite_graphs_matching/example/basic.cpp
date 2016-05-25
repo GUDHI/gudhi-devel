40a293e774742d308ce02416326e67d5375cc371
cjamin <cjamin@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2016-05-25 15:31:07 +0000

    Missing include_directories
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/branches/bottleneckDistance@1203 636b058d-ea47-450e-bf9e-a15bfbe3eedb
    
    Former-commit-id: f75268759affc6ce3e7c5a2929ba00971a1c48a1

--- src/Bipartite_graphs_matching/example/basic.cpp
+++ src/Bipartite_graphs_matching/example/basic.cpp
@@ -20,7 +20,7 @@
  *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "../include/gudhi/Graph_matching.h"
+#include <gudhi/Graph_matching.h>
 #include <iostream>
 
 #include <chrono>
