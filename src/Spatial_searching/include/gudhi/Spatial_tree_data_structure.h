dbe46f1faf07b6cd074881f92dfea4eb5dde38c7
cjamin <cjamin@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2016-06-21 14:01:45 +0000

    Fix/add copyright
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/branches/subsampling_and_spatialsearching@1324 636b058d-ea47-450e-bf9e-a15bfbe3eedb
    
    Former-commit-id: dc76efeb44b94b3a3fc1669d8b6636759ab92383

--- src/Spatial_searching/include/gudhi/Spatial_tree_data_structure.h
+++ src/Spatial_searching/include/gudhi/Spatial_tree_data_structure.h
@@ -4,7 +4,7 @@
  *
  *    Author(s):       Clement Jamin
  *
- *    Copyright (C) 2016  INRIA Sophia-Antipolis (France)
+ *    Copyright (C) 2016 INRIA
  *
  *    This program is free software: you can redistribute it and/or modify
  *    it under the terms of the GNU General Public License as published by
--- src/Subsampling/test/test_sparsify_point_set.cpp
+++ src/Subsampling/test/test_sparsify_point_set.cpp
@@ -1,6 +1,24 @@
-// #ifdef _DEBUG
-// # define TBB_USE_THREADING_TOOL
-// #endif
+/*    This file is part of the Gudhi Library. The Gudhi library
+ *    (Geometric Understanding in Higher Dimensions) is a generic C++
+ *    library for computational topology.
+ *
+ *    Author(s):       Clement Jamin
+ *
+ *    Copyright (C) 2016 INRIA
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE Subsampling - test sparsify_point_set
