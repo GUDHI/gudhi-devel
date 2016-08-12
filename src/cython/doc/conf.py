efd59bb9198c4c60a363505dd6fc71fc5b1d8ecd
vrouvrea <vrouvrea@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2016-08-12 16:01:48 +0000

    gudhi.so is more in ../.. but in ..
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/branches/ST_cythonize@1437 636b058d-ea47-450e-bf9e-a15bfbe3eedb
    
    Former-commit-id: cc66d57288e7559774ea30f38679e4cd172235f1

--- src/cython/doc/conf.py
+++ src/cython/doc/conf.py
@@ -21,7 +21,7 @@ import os
 #sys.path.insert(0, os.path.abspath('.'))
 
 # Path to Gudhi.so from source path
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- General configuration ------------------------------------------------
 
