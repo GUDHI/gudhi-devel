92751f7bb87d45469e260b7e5161bf6e375bf231
vrouvrea <vrouvrea@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2016-08-12 12:50:05 +0000

    Add todo feature to list the todos in a page.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/branches/ST_cythonize@1431 636b058d-ea47-450e-bf9e-a15bfbe3eedb
    
    Former-commit-id: 89ea800ed4fb0b9e821a6c2650ffb79c09744325

--- src/cython/doc/source/conf.py
+++ src/cython/doc/source/conf.py
@@ -42,6 +42,7 @@ extensions = [
     'sphinxcontrib.bibtex',
 ]
 
+todo_include_todos = True
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
