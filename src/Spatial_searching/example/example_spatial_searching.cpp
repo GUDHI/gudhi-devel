a935df397bba093848cfe5905aca1a158294825c
cjamin <cjamin@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2016-09-01 14:50:04 +0000

    Fix sentence
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/branches/subsampling_and_spatialsearching@1469 636b058d-ea47-450e-bf9e-a15bfbe3eedb
    
    Former-commit-id: d66e96bd7638ec76b9136b84695691af4c6a5691

--- src/Spatial_searching/example/example_spatial_searching.cpp
+++ src/Spatial_searching/example/example_spatial_searching.cpp
@@ -25,8 +25,8 @@ int main (void)
 
   Points_ds points_ds(points);
 
-  // 20-nearest neighbor query
-  std::cout << "20 nearest neighbors:\n";
+  // 10-nearest neighbor query
+  std::cout << "10 nearest neighbors from points[20]:\n";
   auto kns_range = points_ds.query_k_nearest_neighbors(points[20], 10, true);
   for (auto const& nghb : kns_range)
     std::cout << nghb.first << " (sq. dist. = " << nghb.second << ")\n";
@@ -38,8 +38,8 @@ int main (void)
   for (auto ins_iterator = ins_range.begin(); ins_iterator->second < 0.5*0.5 ; ++ins_iterator)
     std::cout << ins_iterator->first << " (sq. dist. = " << ins_iterator->second << ")\n";
 
-  // 20-farthest neighbor query
-  std::cout << "20 farthest neighbors:\n";
+  // 10-farthest neighbor query
+  std::cout << "10 farthest neighbors from points[20]:\n";
   auto kfs_range = points_ds.query_k_farthest_neighbors(points[20], 10, true);
   for (auto const& nghb : kfs_range)
     std::cout << nghb.first << " (sq. dist. = " << nghb.second << ")\n";
