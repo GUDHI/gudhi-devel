f07fc696dbb57d162b383f509619b99340f600d2
pdlotko <pdlotko@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2016-09-28 12:51:57 +0000

    a few modifications to persisntence heat maps. Removing PSSK, since it is not needed. PSSk is exactluy the same as any other heat map. The difference is the way it is created. Comparision, averaging or scalar products are the same. That is why I have removed PSSK folder, and add a few other ways of creating heat maps in persistence_heat_maps folder.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/branches/gudhi_stat@1580 636b058d-ea47-450e-bf9e-a15bfbe3eedb
    
    Former-commit-id: 2f8f2bde0fdbe69b582d0afdbb5bf6e4fe3fff31

---
+++ src/Gudhi_stat/example/utilities/persistence_heat_maps/create_persistence_heat_maps_weighted_by_distance_from_diagonal.cpp
@@ -0,0 +1,79 @@
+/*    This file is part of the Gudhi Library. The Gudhi library
+ *    (Geometric Understanding in Higher Dimensions) is a generic C++
+ *    library for computational topology.
+ *
+ *    Author(s):       Pawel Dlotko
+ *
+ *    Copyright (C) 2015  INRIA Sophia-Saclay (France)
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+
+#include <gudhi/abstract_classes/Abs_Topological_data.h>
+#include <gudhi/concretizations/Persistence_heat_maps.h>
+
+
+
+using namespace Gudhi;
+using namespace Gudhi::Gudhi_stat;
+
+#include <iostream>
+#include <sstream>
+
+
+
+int main( int argc , char** argv )
+{
+	std::cout << "This program creates persistence heat map of diagrams provided as an input. The Gaussian kernels are weighted by the distance of a center from the diagonal.\n";
+	std::cout << "The first parameter of a program is an integer, a size of a grid.\n";
+	std::cout << "The second and third parameters are min and max of the grid. If you want those numbers to be computed based on the data, set them both to -1 \n";
+	std::cerr << "The fourth parameter is an integer, the standard deviation of a gaussian kernel expressed in a number of pixels \n";
+	std::cout << "The remaining parameters are the names of files with persistence diagrams. \n";
+	
+	if ( argc < 5 )
+	{
+		std::cout << "Wrong parameter list, the program will now terminate \n";
+		return 1;
+	}
+	
+	size_t size_of_grid = (size_t)atoi( argv[1] );
+	double min_ = atof( argv[2] );
+	double max_ = atof( argv[3] );
+	size_t stdiv = atof( argv[4] );
+	
+	std::vector< const char* > filenames;
+	for ( int i = 5 ; i < argc ; ++i )
+	{
+		filenames.push_back( argv[i] );
+	}
+
+	
+	
+	std::cout << "Creating persistence heat maps...\n";	
+	std::vector< std::vector<double> > filter = create_Gaussian_filter(stdiv,1);
+	
+	for ( size_t i = 0 ; i != filenames.size() ; ++i )
+	{		
+		std::cout << "Creating a heat map based on a file : " << filenames[i] << std::endl;
+		Persistence_heat_maps l( filenames[i] , filter, distance_from_diagonal, false , size_of_grid , min_ , max_ );		
+		
+		std::stringstream ss;
+		ss << filenames[i] << ".mps";
+		l.print_to_file( ss.str().c_str() );
+	}
+	std::cout << "Done \n";
+	return 0;
+}
