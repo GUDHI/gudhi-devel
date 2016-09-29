452cf2181433a317fcc4d8c0661df3401073eec9
vrouvrea <vrouvrea@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2016-09-29 10:56:50 +0000

    New example and texts for doc
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/branches/rips_complex_module@1587 636b058d-ea47-450e-bf9e-a15bfbe3eedb
    
    Former-commit-id: 332335f6f101f08e246b35de6e3a84c2348fd4ee

---
+++ src/Rips_complex/example/example_rips_complex_from_points.cpp
@@ -0,0 +1,69 @@
+#include <gudhi/Rips_complex.h>
+// to construct Rips_complex from a OFF file of points
+#include <gudhi/Points_off_io.h>
+// to construct a simplex_tree from rips complex
+#include <gudhi/Simplex_tree.h>
+#include <gudhi/distance_functions.h>
+
+#include <iostream>
+#include <string>
+#include <limits>  // for std::numeric_limits
+
+void usage(int nbArgs, char * const progName) {
+  std::cerr << "Error: Number of arguments (" << nbArgs << ") is not correct\n";
+  std::cerr << "Usage: " << progName << " dim_max [threshold]\n";
+  std::cerr << "       i.e.: " << progName << " 3 12.0\n";
+  exit(-1);  // ----- >>
+}
+
+int main(int argc, char **argv) {
+  if ((argc != 2) && (argc != 3)) usage(argc, argv[0]);
+
+  double threshold = std::numeric_limits<double>::infinity();
+  int dim_max = atoi(argv[1]);
+  
+  if (argc == 3) {
+    threshold = atof(argv[2]);
+  }
+
+  // Type definitions
+  using Point = std::vector<double>;
+  using Simplex_tree = Gudhi::Simplex_tree<>;
+  using Rips_complex = Gudhi::rips_complex::Rips_complex<Simplex_tree::Filtration_value>;
+
+  std::vector<Point> points;
+  points.push_back({1.0, 1.0});
+  points.push_back({7.0, 0.0});
+  points.push_back({4.0, 6.0});
+  points.push_back({9.0, 6.0});
+  points.push_back({0.0, 14.0});
+  points.push_back({2.0, 19.0});
+  points.push_back({9.0, 17.0});
+  
+  // ----------------------------------------------------------------------------
+  // Init of a rips complex from an OFF file
+  // ----------------------------------------------------------------------------
+  Rips_complex rips_complex_from_file(points, threshold, euclidean_distance<Point>);
+
+  Simplex_tree simplex;
+  if (rips_complex_from_file.create_complex(simplex, dim_max)) {
+    // ----------------------------------------------------------------------------
+    // Display information about the rips complex
+    // ----------------------------------------------------------------------------
+    std::cout << "Rips complex is of dimension " << simplex.dimension() <<
+        " - " << simplex.num_simplices() << " simplices - " <<
+        simplex.num_vertices() << " vertices." << std::endl;
+
+    std::cout << "Iterator on rips complex simplices in the filtration order, with [filtration value]:" <<
+        std::endl;
+    for (auto f_simplex : simplex.filtration_simplex_range()) {
+      std::cout << "   ( ";
+      for (auto vertex : simplex.simplex_vertex_range(f_simplex)) {
+        std::cout << vertex << " ";
+      }
+      std::cout << ") -> " << "[" << simplex.filtration(f_simplex) << "] ";
+      std::cout << std::endl;
+    }
+  }
+  return 0;
+}
