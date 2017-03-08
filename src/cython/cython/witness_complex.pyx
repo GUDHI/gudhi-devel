30890785a1b1516cbcfbf5c9b41e4657794d3fb2
vrouvrea <vrouvrea@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2017-03-08 22:51:17 +0000

    Add witness_complex_from_nearest_landmark_table.py
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/branches/ST_cythonize@2182 636b058d-ea47-450e-bf9e-a15bfbe3eedb
    
    Former-commit-id: 5e9efcf4c6e50f0864cf473d649dfa26b4ef2cf4

--- src/cython/cython/witness_complex.pyx
+++ src/cython/cython/witness_complex.pyx
@@ -44,7 +44,7 @@ cdef class WitnessComplex:
     cdef Witness_complex_interface * thisptr
 
     # Fake constructor that does nothing but documenting the constructor
-    def __init__(self, points=None, number_of_landmarks=5):
+    def __init__(self, nearest_landmark_table=None):
         """WitnessComplex constructor.
 
         :param nearest_landmark_table: A list of nearest landmark.
