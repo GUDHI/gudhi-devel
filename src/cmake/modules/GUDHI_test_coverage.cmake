2f63617adf4ea8b2fe07c6125d7f582b978cb922
vrouvrea <vrouvrea@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2017-04-18 13:24:54 +0000

    Rename CMake tests Add specific GUDHI test coverage function Rename cmake/modules/GUDHI_*.txt
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/branches/add_test_windows@2362 636b058d-ea47-450e-bf9e-a15bfbe3eedb
    
    Former-commit-id: 071b1eda7d11b1781a9601a013b88a0b2a9b7bf7

---
+++ src/cmake/modules/GUDHI_test_coverage.cmake
@@ -0,0 +1,15 @@
+
+if (GCOVR_PATH)
+  # for gcovr to make coverage reports - Corbera Jenkins plugin
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fprofile-arcs -ftest-coverage")
+endif()
+if (GPROF_PATH)
+  # for gprof to make coverage reports - Jenkins
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pg")
+endif()
+
+function(gudhi_add_coverage_test unitary_test)
+  message("++  ${CMAKE_BINARY_DIR}/${unitary_test}_UT.xml")
+  add_test(NAME ${unitary_test} COMMAND $<TARGET_FILE:${unitary_test}>
+      "--log_format=XML" "--log_sink=${CMAKE_BINARY_DIR}/${unitary_test}_UT.xml" "--log_level=test_suite" "--report_level=no")
+endfunction()
