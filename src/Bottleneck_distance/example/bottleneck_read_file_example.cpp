006516d2df5eaa6e38765158f72fa29fea8fb610
cjamin <cjamin@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2017-05-30 12:50:12 +0000

    Remove debug code
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/branches/read_persistence_from_file@2473 636b058d-ea47-450e-bf9e-a15bfbe3eedb
    
    Former-commit-id: 458dcfcf18b36f8dd0023dfe5af0d6083d1c7684

--- src/Bottleneck_distance/example/bottleneck_read_file_example.cpp
+++ src/Bottleneck_distance/example/bottleneck_read_file_example.cpp
@@ -20,8 +20,6 @@
  *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#define CGAL_HAS_THREADS
-
 #include <gudhi/Bottleneck.h>
 #include <gudhi/reader_utils.h>
 #include <iostream>
