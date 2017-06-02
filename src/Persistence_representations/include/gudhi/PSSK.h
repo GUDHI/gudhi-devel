e14afa991e33d1f9010590b634802055ad95dcae
vrouvrea <vrouvrea@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2017-06-02 14:43:30 +0000

    Fix cpplint
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/branches/persistence_representation_integration@2507 636b058d-ea47-450e-bf9e-a15bfbe3eedb
    
    Former-commit-id: 0e623d7fda81eb22a1572fe6f821697d2265de3b

--- src/Persistence_representations/include/gudhi/PSSK.h
+++ src/Persistence_representations/include/gudhi/PSSK.h
@@ -121,8 +121,10 @@ void PSSK::construct(const std::vector<std::pair<double, double> >& intervals_,
 
   for (size_t pt_nr = 0; pt_nr != intervals_.size(); ++pt_nr) {
     // compute the value of intervals_[pt_nr] in the grid:
-    int x_grid = static_cast<int>((intervals_[pt_nr].first - this->min_) / (this->max_ - this->min_) * number_of_pixels);
-    int y_grid = static_cast<int>((intervals_[pt_nr].second - this->min_) / (this->max_ - this->min_) * number_of_pixels);
+    int x_grid = static_cast<int>((intervals_[pt_nr].first - this->min_) /
+                                  (this->max_ - this->min_) * number_of_pixels);
+    int y_grid = static_cast<int>((intervals_[pt_nr].second - this->min_) /
+                                  (this->max_ - this->min_) * number_of_pixels);
 
     if (dbg) {
       std::cerr << "point : " << intervals_[pt_nr].first << " , " << intervals_[pt_nr].second << std::endl;
