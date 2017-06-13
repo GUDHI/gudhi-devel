e6b1730fd0832fad2a9103863428465155f70986
cjamin <cjamin@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2017-06-13 15:29:35 +0000

    This should never have been commented out
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/branches/read_persistence_from_file@2540 636b058d-ea47-450e-bf9e-a15bfbe3eedb
    
    Former-commit-id: 5e5fc971986908e5b9383130704536686e1b66ea

--- src/Bottleneck_distance/example/bottleneck_read_file_example.cpp
+++ src/Bottleneck_distance/example/bottleneck_read_file_example.cpp
@@ -34,7 +34,7 @@ int main(int argc, char** argv) {
     std::cout << "To run this program please provide as an input two files with persistence diagrams. Each file " <<
         "should contain a birth-death pair per line. Third, optional parameter is an error bound on a bottleneck" <<
         " distance (set by default to zero). The program will now terminate \n";
-    //return -1;
+    return -1;
   }
   std::vector<std::pair<double, double>> diag1 = read_persistence_intervals_in_dimension(argv[1]);
   std::vector<std::pair<double, double>> diag2 = read_persistence_intervals_in_dimension(argv[2]);
