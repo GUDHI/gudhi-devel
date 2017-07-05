40aaa716132bb2f6a6110e229d91345618bf1088
vrouvrea <vrouvrea@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2017-07-05 17:50:45 +0000

    Checkin tests and fixes
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/branches/graph_expansion_with_blocker_oracle@2586 636b058d-ea47-450e-bf9e-a15bfbe3eedb
    
    Former-commit-id: 0f9cbe3d6b94e939e151dea989ae99f7e4f3bffa

--- src/Simplex_tree/example/graph_expansion_with_blocker.cpp
+++ src/Simplex_tree/example/graph_expansion_with_blocker.cpp
@@ -20,7 +20,6 @@
  *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <gudhi/graph_simplicial_complex.h>
 #include <gudhi/Simplex_tree.h>
 
 #include <iostream>
