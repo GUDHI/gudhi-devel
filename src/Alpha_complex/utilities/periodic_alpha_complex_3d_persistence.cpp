0a0b42dc45ddcc5065cf70dc05e7070ce09eeff5
glisse <glisse@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2017-11-30 15:41:18 +0000

    dead typedef
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/branches/misc-glisse@2991 636b058d-ea47-450e-bf9e-a15bfbe3eedb
    
    Former-commit-id: a314bb92443fc49370a7e12da84713eed7985981

--- src/Alpha_complex/utilities/periodic_alpha_complex_3d_persistence.cpp
+++ src/Alpha_complex/utilities/periodic_alpha_complex_3d_persistence.cpp
@@ -79,7 +79,6 @@ using ST = Gudhi::Simplex_tree<Gudhi::Simplex_tree_options_fast_persistence>;
 using Filtration_value = ST::Filtration_value;
 using Simplex_tree_vertex = ST::Vertex_handle;
 using Alpha_shape_simplex_tree_map = std::map<Alpha_shape_3::Vertex_handle, Simplex_tree_vertex>;
-using Alpha_shape_simplex_tree_pair = std::pair<Alpha_shape_3::Vertex_handle, Simplex_tree_vertex>;
 using Simplex_tree_vector_vertex = std::vector<Simplex_tree_vertex>;
 using Persistent_cohomology =
     Gudhi::persistent_cohomology::Persistent_cohomology<ST, Gudhi::persistent_cohomology::Field_Zp>;
