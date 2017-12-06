36d541b59efcff02d4b80f11724ba78360d4d1bc
vrouvrea <vrouvrea@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2017-12-06 11:52:35 +0000

    Fix precision error bug
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/trunk@3047 636b058d-ea47-450e-bf9e-a15bfbe3eedb
    
    Former-commit-id: f1fb36cb9f658b53aaa5844c24eef404556003fd

--- src/Persistence_representations/test/persistence_lanscapes_test.cpp
+++ src/Persistence_representations/test/persistence_lanscapes_test.cpp
@@ -220,7 +220,7 @@ BOOST_AUTO_TEST_CASE(check_computations_of_distances) {
   std::cout << "p.distance(q) = " << p.distance(q) << std::endl;
   BOOST_CHECK(fabs(p.distance(q) - 25.5824) <= 0.00005);
   std::cout << "p.distance(q, 2) = " << p.distance(q, 2) << std::endl;
-  BOOST_CHECK(fabs(p.distance(q, 2) - 2.12636) <= 0.00001);
+  BOOST_CHECK(fabs(p.distance(q, 2) - 2.1264) <= 0.0001);
   std::cout << "p.distance(q, std::numeric_limits<double>::max()) = " <<
                p.distance(q, std::numeric_limits<double>::max()) << std::endl;
   BOOST_CHECK(fabs(p.distance(q, std::numeric_limits<double>::max()) - 0.359068) <= 0.00001);
