a0116bde72e9becc10a14b171089fe15a3d53c06
vrouvrea <vrouvrea@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2018-02-14 08:51:08 +0000

    Fix copyright issues for Simplex tree
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/branches/cechcomplex_vincent@3248 636b058d-ea47-450e-bf9e-a15bfbe3eedb
    
    Former-commit-id: d13758f0059174863de260efac88b32aef256753

--- src/Simplex_tree/example/mini_simplex_tree.cpp
+++ src/Simplex_tree/example/mini_simplex_tree.cpp
@@ -4,7 +4,7 @@
  *
  *    Author(s):       Marc Glisse
  *
- *    Copyright (C) 2015  INRIA Saclay - Ile-de-France (France)
+ *    Copyright (C) 2015  Inria
  *
  *    This program is free software: you can redistribute it and/or modify
  *    it under the terms of the GNU General Public License as published by
