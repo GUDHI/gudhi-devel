e44472f472b026078cebdd7c15984ac1310b5588
skachano <skachano@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2018-04-12 15:44:14 +0000

    add output_points_to_medit.h
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/branches/cc_wit_hiraoka@3380 636b058d-ea47-450e-bf9e-a15bfbe3eedb
    
    Former-commit-id: 5a272bbd6a7144beb41ff9981c1b6f8a632918e2

---
+++ src/Coxeter_triangulation/example/output_points_to_medit.h
@@ -0,0 +1,37 @@
+#ifndef OUTPUT_POINTS_TO_MEDIT_
+#define OUTPUT_POINTS_TO_MEDIT_
+
+template <class Point_range>          
+void output_points_to_medit(Point_range& range, std::string file_name = "points.mesh") {
+  short d = range.begin()->size();
+  if (d > 3);
+
+  std::ofstream ofs (file_name, std::ofstream::out);
+  if (d <= 2)
+    ofs << "MeshVersionFormatted 1\nDimension 2\n";
+  else
+    ofs << "MeshVersionFormatted 1\nDimension 3\n";
+
+  ofs << "Vertices\n" << (d+1)*range.size() << "\n";
+  for (auto v: range) 
+    for (int i = 0; i < d+1; ++i) {
+      for (auto x: v)
+        ofs << x << " ";
+      ofs << "\n";
+    }
+  if (d == 2) {
+    ofs << "Edges\n" << 3*range.size() << "\n";
+    for (unsigned k = 0; k < range.size(); ++k) {
+      ofs << 3*k+1 << " " << 3*k+2 << "\n";
+      ofs << 3*k+1 << " " << 3*k+3 << "\n";
+      ofs << 3*k+2 << " " << 3*k+3 << "\n";
+    }
+    ofs << "Triangles\n" << range.size() << "\n";
+    for (unsigned k = 0; k < range.size(); ++k)
+      ofs << 3*k+1 << " " << 3*k+2 << " " << 3*k+3 << "\n";
+  }
+
+  ofs.close();
+}
+
+#endif
