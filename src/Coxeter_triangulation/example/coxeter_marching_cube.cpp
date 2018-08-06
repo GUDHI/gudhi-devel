48a3637732c863e60005cbf50fbe6cc72e97701a
skachano <skachano@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2018-08-06 10:08:22 +0000

    Added the marching cube file
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/branches/cc_wit_hiraoka@3740 636b058d-ea47-450e-bf9e-a15bfbe3eedb
    
    Former-commit-id: a1fc95e4dcd024318f47a61cc776f36a7d7e55d2

---
+++ src/Coxeter_triangulation/example/coxeter_marching_cube.cpp
@@ -0,0 +1,33 @@
+#define CC_STAR_COMPLETION
+// #define CC_A_V_VISITORS
+
+#include <iostream>
+#include <vector>
+
+// #include <gudhi/Points_off_io.h>
+// #include <gudhi/Coxeter_system.h>
+// #include <gudhi/Coxeter_complex.h>
+// #include <gudhi/Coxeter_complex/Off_point_range.h>
+// #include <gudhi/Clock.h>
+
+#include <CGAL/Epick_d.h>
+
+// #include "memory_usage.h"
+// #include "cxx-prettyprint/prettyprint.hpp"
+// #include "output_points_to_medit.h"
+
+using K = CGAL::Epick_d<CGAL::Dynamic_dimension_tag>;
+// using FT = K::FT;
+using Point_d = K::Point_d;
+// using Point_vector = std::vector< Point_d >;
+// using Coxeter_complex = Gudhi::Coxeter_complex<Point_vector, Coxeter_system>;
+
+void seed_expansion(const Point_d& p) {
+
+}
+
+int main(int argc, char * const argv[]) {
+  std::vector<Point_d> seed_points;
+  for (const auto& p: seed_points)
+    seed_expansion(p);
+}
