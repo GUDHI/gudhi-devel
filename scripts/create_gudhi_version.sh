6a52e22654a677f087267226366de02ab7bb7527
vrouvrea <vrouvrea@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2018-08-31 12:24:55 +0000

    Add script to automize gudhi version creation
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/trunk@3860 636b058d-ea47-450e-bf9e-a15bfbe3eedb
    
    Former-commit-id: fc01077dc2120e5c5ad2190a5f0444859ecee426

---
+++ scripts/create_gudhi_version.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+login="vrouvrea"
+version="2.3.0.rc3"
+cgaldir="/home/vincent/workspace/CGAL-4.11-HO/build"
+cpucount=7
+
+
+# We start from scripts dir in the dev branch
+cd ..
+RELATIVEURL=`svn info . |grep -F "Relative URL:" | awk '{print $NF}'`
+
+if [ "$RELATIVEURL" != "^/trunk" ]
+then
+echo "Script must be launched in trunk and not in $RELATIVEURL"
+exit
+fi
+
+rm -rf build; mkdir build; cd build; cmake  -DCMAKE_BUILD_TYPE=Debug -DDEBUG_TRACES=ON -DCGAL_DIR=${cgaldir} -DWITH_GUDHI_EXAMPLE=ON -DWITH_GUDHI_BENCHMARK=ON -DPython_ADDITIONAL_VERSIONS=3 ..
+cmake  -DCMAKE_BUILD_TYPE=Debug .
+
+make -j ${cpucount} all test
+
+cd ..
+svn st | grep -v GUDHIVersion.cmake | grep "^\?" | awk "{print \$2}" | xargs rm -rf
+
+svn copy svn+ssh://${login}@scm.gforge.inria.fr/svnroot/gudhi/trunk svn+ssh://${login}@scm.gforge.inria.fr/svnroot/gudhi/tags/gudhi-release-${version} \
+  -m "Creating a tag of Gudhi release version ${version}."
+
+cd build
+make user_version
+
+userversiondir=`find . -type d -name "*_GUDHI_${version}" | sed 's/\.\///g'`
+echo "User version directory = ${userversiondir}"
+
+tar -czvf ${userversiondir}.tar.gz ${userversiondir}
+
+userdocdir=${userversiondir/GUDHI/GUDHI_DOC}
+echo "User documentation directory = ${userdocdir}"
+mkdir ${userdocdir}
+make doxygen
+
+cp -R ${userversiondir}/doc/html ${userdocdir}/cpp
+cd ${userversiondir}
+rm -rf build; mkdir build; cd build; cmake  -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=./installed -DCGAL_DIR=${cgaldir} -DWITH_GUDHI_EXAMPLE=ON -DPython_ADDITIONAL_VERSIONS=3 ..
+make sphinx
+
+cp -R cython/sphinx ../../${userdocdir}/python
+cd ../..
+tar -czvf ${userdocdir}.tar.gz ${userdocdir}
+
+cd ${userversiondir}/build
+make -j ${cpucount} all test install
+
+cd ../..
+actualdir=`pwd`
+echo "Library is available at ${actualdir}/${userversiondir}.tar.gz"
+sha256sum ${userversiondir}.tar.gz
+echo "Documentation is available at ${actualdir}/${userdocdir}.tar.gz"
