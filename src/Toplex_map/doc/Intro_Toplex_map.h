11b1de2771b060fb286bf428ac081c07ff73e8ec
vrouvrea <vrouvrea@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2018-11-16 08:20:00 +0000

    Doc review : Gamma_0 instead of gamma_0 to be like in the paper              toplices is the plural form of toplex (not a set of toplex)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/branches/toplex_map@3984 636b058d-ea47-450e-bf9e-a15bfbe3eedb
    
    Former-commit-id: 4cfeebffcbf9006fd98bbf32a8dbd9eb683c9bf5

--- src/Toplex_map/doc/Intro_Toplex_map.h
+++ src/Toplex_map/doc/Intro_Toplex_map.h
@@ -34,15 +34,15 @@ namespace Gudhi {
  * \section toplexmapdefinition Definition
  *
  * A Toplex_map is a data structure to represent and store a simplicial complex. A "toplex" is the contraction of
- * "top-simplex", also known as a maximal simplex. We will call "toplices" a set of "toplex".
+ * "top-simplex", also known as a maximal simplex. The plural form of "toplex" will be called "toplices".
  *
  * Let's consider a simplicial complex, denote by \f$d\f$ its dimension and by \f$k\f$ its number of maximal simplices.
- * Furthermore, denote by \f$\gamma_0\f$ the maximal number of toplices, i.e. maximal simplices, that contain a same
+ * Furthermore, denote by \f$\Gamma_0\f$ the maximal number of toplices, i.e. maximal simplices, that contain a same
  * vertex.
  *
  * The goal of the Toplex Map is both to represent the complex in optimal O(kd) space and to provide fast standard
  * operations such as : insertion, removal, contraction of an edge, collapses and membership of a simplex. The time
- * needed for these operation is linear or quadratic in \f$\gamma_0\f$ and \f$d\f$.
+ * needed for these operation is linear or quadratic in \f$\Gamma_0\f$ and \f$d\f$.
  *
  * Toplex map is composed firstly of a raw storage of toplices and secondly of a map which associate any vertex to a
  * set of pointers toward all toplices containing this vertex.
