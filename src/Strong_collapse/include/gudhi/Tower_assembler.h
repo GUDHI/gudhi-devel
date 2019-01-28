2f93c171d851ef805f7c4de3d29d9da555bc6967
vrouvrea <vrouvrea@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2019-01-28 11:31:24 +0000

    Clang format files Remove unused using/typedefs Remove useless pointers constify get methods
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/branches/strong_collapse_vincent@4077 636b058d-ea47-450e-bf9e-a15bfbe3eedb
    
    Former-commit-id: 2405e57601dcef3b1fc44f5fce94dc66e26c40c2

--- src/Strong_collapse/include/gudhi/Tower_assembler.h
+++ src/Strong_collapse/include/gudhi/Tower_assembler.h
@@ -32,16 +32,8 @@
 #include <algorithm>
 #include <limits>
 
+using Distance_matrix = std::vector<std::vector<double>>;
 
-typedef int Vertex;
-using Edge                  = std::pair<Vertex,Vertex>;
-using Edge_list             = std::vector<Edge>;
-using Simplex               = std::vector<Vertex>;
-
-using vectorVertex          = std::vector<Vertex>;
-using vert_unSet            = std::unordered_set<Vertex>;
-using Map                   = std::unordered_map<Vertex,Vertex>;
-using Distance_matrix       = std::vector<std::vector<double>>;
 // using infinity              = std::numeric_limits<double>::max();
 // assumptions : (1) K1 and K2 have the same vertex set
 //               (2) The set of simplices of K1 is a subset of set of simplices of K2
@@ -50,121 +42,115 @@ using Distance_matrix       = std::vector<std::vector<double>>;
 // |       |
 // K1c ->  K2c   [Strongly Collapsed Flag Complexes]
 
-class Tower_assembler
-{
-  private:
-	Map renamedVertices; 
-    Map representative_map;
-	std::size_t current_rename_counter;
-    size_t total_vertices;
+class Tower_assembler {
+ private:
+  Map renamedVertices;
+  Map representative_map;
+  std::size_t current_rename_counter;
+  size_t total_vertices;
 
-    Flag_complex_sparse_matrix * flag_Filtration;
-    typedef std::vector< std::tuple< double, Vertex, Vertex > > Filtered_sorted_edge_list;
+  Flag_complex_sparse_matrix flag_filtration_;
 
-  public:
-    Tower_assembler(std::size_t numVert)
-    {
-    	for (std::size_t i = 0; i <= numVert; ++i){
-    		renamedVertices[i] = i;
-        }
-    	total_vertices = 0;
-        current_rename_counter = numVert+1;
-        
-        flag_Filtration = new Flag_complex_sparse_matrix(numVert);
+ public:
+  Tower_assembler(std::size_t numVert) : flag_filtration_(numVert) {
+    for (std::size_t i = 0; i <= numVert; ++i) {
+      renamedVertices[i] = i;
     }
+    total_vertices = 0;
+    current_rename_counter = numVert + 1;
+  }
 
-    void build_tower_for_two_cmplxs(Flag_complex_sparse_matrix mat_1, const Flag_complex_sparse_matrix & mat_2,  Map redmap_2,  double filtration_value, std::string outFile) // mat_1 and mat_2 are simplex_trees of K1c and K2c (the collapsed ones), redmap_2 is the map of K2 -> K2c
-    {
-        std::ofstream myfile (outFile, std::ios::app);
-        if (myfile.is_open())
-        {   
-            for (auto & v : mat_1.vertex_set()) {
-                auto collapsed_to = redmap_2.find(v);  // If v collapsed to something?
-                if(collapsed_to != redmap_2.end()) {  // Collapse happened, because there is a vertex in the map
-                    if(mat_1.membership(collapsed_to->second)) { // Collapsed to an existing vertex in mat_1.
-                        flag_Filtration->active_strong_expansion(renamedVertices.at(v), renamedVertices.at(collapsed_to->second), filtration_value);
-                        renamedVertices.at(v) = current_rename_counter;
-                        current_rename_counter++;                        
-                    }
-                    else {
-	                    myfile << filtration_value << " i " << renamedVertices.at(collapsed_to->second) << std::endl;
-	                    myfile  << filtration_value << " c " << renamedVertices.at(v) << " " << renamedVertices.at(collapsed_to->second) << std::endl; 
-	                    flag_Filtration->active_strong_expansion(renamedVertices.at(v), renamedVertices.at(collapsed_to->second),filtration_value);
-                      renamedVertices.at(v) = current_rename_counter;
-	                    current_rename_counter++;
-                    }
-                     // If the vertex "collapsed_to->second" is not a member of mat_1, the contraction function will simply add and then collapse
-                     mat_1.contraction(v, collapsed_to->second);
-                }
-            }
-
-            //The core K1c (mat_1) has gone through the transformation(re-labeling)/collapse and it is now a subcomplex of K2c, the remaining simplices need to be included
-            // Writing the inclusion of all remaining simplices...
-            for( const Edge & e  : mat_2.all_edges()) {
-                auto u = std::get<0>(e);
-                auto v = std::get<1>(e);
-                if(!mat_1.membership(u)) {
-                    flag_Filtration->insert_vertex(renamedVertices.at(u),filtration_value);
-                    myfile << filtration_value << " i";
-                    myfile  << " " << renamedVertices.at(u);
-                    myfile  << std::endl;
-                    mat_1.insert_vertex(u,1);
-                }
-
-                if(!mat_1.membership(v)) {
-                    flag_Filtration->insert_vertex(renamedVertices.at(v),filtration_value);
-
-                    myfile << filtration_value << " i";
-                    myfile  << " " << renamedVertices.at(v);
-                    myfile  << std::endl;
-                    mat_1.insert_vertex(v,1);
-                }
-                if(!mat_1.membership(e)){
-                    flag_Filtration->insert_new_edges(renamedVertices.at(u),renamedVertices.at(v), filtration_value);
-
-                    myfile << filtration_value << " i";
-                    myfile  << " " <<  renamedVertices.at(u) << ", " << renamedVertices.at(v);
-                    myfile  << std::endl;
-                    mat_1.insert_new_edges(u,v,1);
-                }
-            }                        
-            myfile << "# Tower updated for the additional subcomplex.\n";
-            myfile.close();
+  void build_tower_for_two_cmplxs(Flag_complex_sparse_matrix mat_1, const Flag_complex_sparse_matrix& mat_2,
+                                  Map redmap_2, double filtration_value,
+                                  std::string outFile)  // mat_1 and mat_2 are simplex_trees of K1c and K2c (the
+                                                        // collapsed ones), redmap_2 is the map of K2 -> K2c
+  {
+    std::ofstream myfile(outFile, std::ios::app);
+    if (myfile.is_open()) {
+      for (auto& v : mat_1.vertex_set()) {
+        auto collapsed_to = redmap_2.find(v);            // If v collapsed to something?
+        if (collapsed_to != redmap_2.end()) {            // Collapse happened, because there is a vertex in the map
+          if (mat_1.membership(collapsed_to->second)) {  // Collapsed to an existing vertex in mat_1.
+            flag_filtration_.active_strong_expansion(renamedVertices.at(v), renamedVertices.at(collapsed_to->second),
+                                                     filtration_value);
+            renamedVertices.at(v) = current_rename_counter;
+            current_rename_counter++;
+          } else {
+            myfile << filtration_value << " i " << renamedVertices.at(collapsed_to->second) << std::endl;
+            myfile << filtration_value << " c " << renamedVertices.at(v) << " "
+                   << renamedVertices.at(collapsed_to->second) << std::endl;
+            flag_filtration_.active_strong_expansion(renamedVertices.at(v), renamedVertices.at(collapsed_to->second),
+                                                     filtration_value);
+            renamedVertices.at(v) = current_rename_counter;
+            current_rename_counter++;
+          }
+          // If the vertex "collapsed_to->second" is not a member of mat_1, the contraction function will simply add and
+          // then collapse
+          mat_1.contraction(v, collapsed_to->second);
         }
-        else {
-            std::cerr << "Unable to open file " << outFile;
-            exit(-1) ;
+      }
+
+      // The core K1c (mat_1) has gone through the transformation(re-labeling)/collapse and it is now a subcomplex of
+      // K2c, the remaining simplices need to be included
+      // Writing the inclusion of all remaining simplices...
+      for (const Edge& e : mat_2.all_edges()) {
+        auto u = std::get<0>(e);
+        auto v = std::get<1>(e);
+        if (!mat_1.membership(u)) {
+          flag_filtration_.insert_vertex(renamedVertices.at(u), filtration_value);
+          myfile << filtration_value << " i";
+          myfile << " " << renamedVertices.at(u);
+          myfile << std::endl;
+          mat_1.insert_vertex(u, 1);
         }
+
+        if (!mat_1.membership(v)) {
+          flag_filtration_.insert_vertex(renamedVertices.at(v), filtration_value);
+
+          myfile << filtration_value << " i";
+          myfile << " " << renamedVertices.at(v);
+          myfile << std::endl;
+          mat_1.insert_vertex(v, 1);
+        }
+        if (!mat_1.membership(e)) {
+          flag_filtration_.insert_new_edges(renamedVertices.at(u), renamedVertices.at(v), filtration_value);
+
+          myfile << filtration_value << " i";
+          myfile << " " << renamedVertices.at(u) << ", " << renamedVertices.at(v);
+          myfile << std::endl;
+          mat_1.insert_new_edges(u, v, 1);
+        }
+      }
+      myfile << "# Tower updated for the additional subcomplex.\n";
+      myfile.close();
+    } else {
+      std::cerr << "Unable to open file " << outFile;
+      exit(-1);
     }
-    Distance_matrix distance_matrix()
-    {
-        size_t non_zero_rw = flag_Filtration->num_vertices();
-        double inf = std::numeric_limits<double>::max();
-        sparseRowMatrix mat = flag_Filtration->uncollapsed_matrix();
+  }
+  Distance_matrix distance_matrix() {
+    size_t non_zero_rw = flag_filtration_.num_vertices();
+    double inf = std::numeric_limits<double>::max();
+    sparseRowMatrix mat = flag_filtration_.uncollapsed_matrix();
 
-        Distance_matrix distance_mat;
-        for(size_t indx = 0; indx < non_zero_rw; indx++){
-            doubleVector distances ;
-            rowInnerIterator it(mat, indx);
-            for(size_t j = 0; j <= indx; j++) {             // Iterate over the non-zero columns
-                if(it.index() == j && j != indx){
-                    distances.push_back(it.value());  // inner index, here it is equal to it.columns()
-                    ++it;
-                }
-                else if( j == indx)
-                    distances.push_back(0);
-                else
-                     distances.push_back(inf);
-
-            }
-            distance_mat.push_back(distances);
-        }
-        return distance_mat;
-    }      
-   void print_sparse_matrix(){
-        flag_Filtration->print_sparse_skeleton();
-   }
-    
+    Distance_matrix distance_mat;
+    for (size_t indx = 0; indx < non_zero_rw; indx++) {
+      doubleVector distances;
+      rowInnerIterator it(mat, indx);
+      for (size_t j = 0; j <= indx; j++) {  // Iterate over the non-zero columns
+        if (it.index() == j && j != indx) {
+          distances.push_back(it.value());  // inner index, here it is equal to it.columns()
+          ++it;
+        } else if (j == indx)
+          distances.push_back(0);
+        else
+          distances.push_back(inf);
+      }
+      distance_mat.push_back(distances);
+    }
+    return distance_mat;
+  }
+  void print_sparse_matrix() { flag_filtration_.print_sparse_skeleton(); }
 };
 
 #endif  // TOWER_ASSEMBLER_H_
