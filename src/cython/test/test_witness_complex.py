08ddc6dbbbb1cf01a05ab23ece47c3d8cf9957ea
ROUVREAU Vincent <vincent.rouvreau@inria.fr>
2019-06-13 17:07:09 +0200

    Change Copyright

--- src/cython/test/test_witness_complex.py
+++ src/cython/test/test_witness_complex.py
@@ -1,25 +1,13 @@
 from gudhi import WitnessComplex, StrongWitnessComplex, SimplexTree
 
-"""This file is part of the Gudhi Library. The Gudhi library
-   (Geometric Understanding in Higher Dimensions) is a generic C++
-   library for computational topology.
-
-   Author(s):       Vincent Rouvreau
+"""This file is part of the Gudhi Library - https://gudhi.inria.fr/ - which is released under MIT.
+    See file LICENSE or go to https://gudhi.inria.fr/licensing/ for full license details.
+    Author(s):       Vincent Rouvreau
 
    Copyright (C) 2016 Inria
 
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+   Modification(s):
+     - YYYY/MM Author: Description of the modification
 """
 
 __author__ = "Vincent Rouvreau"
