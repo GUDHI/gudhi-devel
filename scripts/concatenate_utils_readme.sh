30e2090066fae2734828c78fdd070a2559da135f
ROUVREAU Vincent <vincent.rouvreau@inria.fr>
2019-06-18 16:09:29 +0200

    scripts cleanup

--- scripts/concatenate_utils_readme.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rm README_FOR_UTILITIES.txt
-locate utilities/README | grep `svn info | grep '^URL:' | egrep -o '(tags|branches)/[^/]+|trunk' | egrep -o '[^/]+$'`  | xargs cat -- >> README_FOR_UTILITIES.txt
