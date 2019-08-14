331fee1010245f487abfa0381dd9e9936654a016
ROUVREAU Vincent <vincent.rouvreau@inria.fr>
2019-08-14 08:21:22 +0200

    Fix #35 Modify Copyright as GUDHI is not only a C++ library

--- src/Simplex_tree/test/simplex_tree_graph_expansion_unit_test.cpp
+++ src/Simplex_tree/test/simplex_tree_graph_expansion_unit_test.cpp
@@ -1,3 +1,13 @@
+/*    This file is part of the Gudhi Library - https://gudhi.inria.fr/ - which is released under MIT.
+ *    See file LICENSE or go to https://gudhi.inria.fr/licensing/ for full license details.
+ *    Author(s):       Vincent Rouvreau
+ *
+ *    Copyright (C) 2014 Inria
+ *
+ *    Modification(s):
+ *      - YYYY/MM Author: Description of the modification
+ */
+
 #include <iostream>
 #include <fstream>
 #include <string>
