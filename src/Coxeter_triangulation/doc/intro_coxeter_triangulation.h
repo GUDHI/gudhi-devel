b6701adba532f49a2e1fed363c67ce0922686f1d
Siargey Kachanovich <siargey.kachanovich@inria.fr>
2019-10-13 11:43:16 +0300

    Added the intro file for the documentation.

---
+++ src/Coxeter_triangulation/doc/intro_coxeter_triangulation.h
@@ -0,0 +1,45 @@
+/*    This file is part of the Gudhi Library. The Gudhi library
+ *    (Geometric Understanding in Higher Dimensions) is a generic C++
+ *    library for computational topology.
+ *
+ *    Author(s):       Siargey Kachanovich
+ *
+ *    Copyright (C) 2019 Inria
+ *
+ *    Modification(s):
+ *      - YYYY/MM Author: Description of the modification
+ */
+
+#ifndef DOC_TANGENTIAL_COMPLEX_INTRO_COXETER_TRIANGULATION_H_
+#define DOC_TANGENTIAL_COMPLEX_INTRO_COXETER_TRIANGULATION_H_
+
+// needs namespaces for Doxygen to link on classes
+namespace Gudhi {
+namespace tangential_complex {
+
+/**  \defgroup coxter_triangulation Coxeter triangulation
+
+\author    Siargey Kachanovich
+
+@{
+
+\section Module overview
+
+Coxeter triangulation module is designed to provide tools for constructing a piecewise-linear approximation of an \f$m\f$-dimensional smooth manifold embedded in the \f$d\f$-dimensional Euclidean space.
+
+\section ambienttriangulations Ambient triangulations
+
+\section intersectionoracle Implicit manifold intersection oracle
+
+\section manifoldtracing Manifold tracing algorithm
+
+\section cellcomplex Cell complex construction
+
+ */
+/** @} */  // end defgroup coxeter_triangulation
+
+}  // namespace coxeter_triangulation
+
+}  // namespace Gudhi
+
+#endif  // DOC_TANGENTIAL_COMPLEX_INTRO_TANGENTIAL_COMPLEX_H_
