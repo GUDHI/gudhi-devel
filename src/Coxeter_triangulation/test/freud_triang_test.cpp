a185344b003cb52aa9a701f773084774fd02bed3
Siargey Kachanovich <siargey.kachanovich@inria.fr>
2019-10-16 14:03:09 +0200

    Rewritten freud_triang_test.

--- src/Coxeter_triangulation/test/freud_triang_test.cpp
+++ src/Coxeter_triangulation/test/freud_triang_test.cpp
@@ -2,6 +2,7 @@
 #define BOOST_TEST_MODULE "permutahedral_representation"
 #include <boost/test/unit_test.hpp>
 
+#include <gudhi/Unitary_tests_utils.h>
 #include <gudhi/Freudenthal_triangulation.h>
 #include <gudhi/Coxeter_triangulation.h>
 
@@ -12,65 +13,80 @@ BOOST_AUTO_TEST_CASE(permutahedral_representation) {
   typedef Gudhi::coxeter_triangulation::Freudenthal_triangulation<> FK_triangulation;
   typedef typename FK_triangulation::Simplex_handle Simplex_handle;
   typedef typename FK_triangulation::Vertex_handle Vertex_handle;
-  typedef typename Simplex_handle::OOrderedSetPartition Ordered_set_partition; 
+  typedef typename Simplex_handle::OrderedSetPartition Ordered_set_partition; 
   typedef typename Ordered_set_partition::value_type Part;
 
   FK_triangulation tr(3);
 
-  // Point location check  
-  Point point({3,-1,0});
+  // Point location check
+  {
+    Point point({3, -1, 0});
+    Simplex_handle s = tr.locate_point(point);
+    BOOST_CHECK( s.vertex() == Vertex_handle({3, -1, 0}) );
+    BOOST_CHECK( s.partition() == Ordered_set_partition({Part({0, 1, 2, 3})}) );
+  }
+
+  {
+    Point point({3.5, -1.5, 0.5});
+    Simplex_handle s = tr.locate_point(point);
+    BOOST_CHECK( s.vertex() == Vertex_handle({3, -2, 0}) );
+    BOOST_CHECK( s.partition() == Ordered_set_partition({Part({0, 1, 2}), Part({3})}) );
+  }
+
+  {
+    Point point({3.5, -1.8, 0.5});
+    Simplex_handle s = tr.locate_point(point);
+    BOOST_CHECK( s.vertex() == Vertex_handle({3, -2, 0}) );
+    BOOST_CHECK( s.partition() == Ordered_set_partition({Part({0, 2}), Part({1}), Part({3})}) );
+  }
+
+  {
+    Point point({3.5, -1.8, 0.3});
+    Simplex_handle s = tr.locate_point(point);
+    BOOST_CHECK( s.vertex() == Vertex_handle({3, -2, 0}) );
+    BOOST_CHECK( s.partition() == Ordered_set_partition({Part({0}), Part({2}), Part({1}), Part({3})}) );
+  }
+
+  // Dimension check
+  BOOST_CHECK( tr.dimension() == 3 );
+  // Matrix check
+  Eigen::MatrixXd default_matrix = Eigen::MatrixXd::Identity(3, 3);  
+  BOOST_CHECK( tr.matrix() == default_matrix );
+  // Vector check
+  Eigen::MatrixXd default_offset = Eigen::VectorXd::Zero(3);  
+  BOOST_CHECK( tr.offset() == default_offset );
+
+  // Barycenter check
+  Point point({3.5, -1.8, 0.3});
   Simplex_handle s = tr.locate_point(point);
-  BOOST_CHECK(s.vertex() == Vertex_handle({3, -1, 0}));
-  BOOST_CHECK(s.vertex() == Ordered_partition(Part({0, 1, 2, 3})));
+  Eigen::Vector3d barycenter_cart = Eigen::Vector3d::Zero();
+  for (auto v: s.vertex_range())
+    for (std::size_t i = 0; i < v.size(); i++)
+      barycenter_cart(i) += v[i];
+  barycenter_cart /= 4.; // simplex is three-dimensional
+  Eigen::Vector3d barycenter = tr.barycenter(s);
+  for (std::size_t i = 0; i < barycenter.size(); i++)
+    GUDHI_TEST_FLOAT_EQUALITY_CHECK(barycenter(i), barycenter_cart(i), 1e-7);
 
-  // std::cout << tr.locate_point(point) << "\n";
-  // std::vector<double> point2({3.5,-1.5,0.5});
-  // std::cout << tr.locate_point(point2) << "\n";
-  // std::vector<double> point3({3.5,-1.8,0.5});
-  // std::cout << tr.locate_point(point3) << "\n";
-  // std::vector<double> point4({3.5,-1.8,0.3});
-  // std::cout << tr.locate_point(point4) << "\n";
-
-  // std::cout << "Triangulation's dimension is " << tr.dimension() << "\n";
-  // std::cout << "Triangulation's matrix is\n" << tr.matrix() << "\n";
-  // std::cout << "Triangulation's offset is\n" << tr.offset() << "\n";
-  // Eigen::MatrixXd new_matrix(3,3);
-  // new_matrix << 1, 0, 0, -1, 1, 0, -1, 0, 1;
-  // std::cout << "Determinant of the new matrix is " << new_matrix.determinant() << "\n";
-  // Eigen::Vector3d new_offset(1.5, 1, 0.5);
+  // Barycenter check for twice the scale
+  s = tr.locate_point(point, 2);
+  barycenter_cart = Eigen::Vector3d::Zero();
+  for (auto v: s.vertex_range())
+    for (std::size_t i = 0; i < v.size(); i++)
+      barycenter_cart(i) += v[i];
+  barycenter_cart /= 3.; // simplex is now a two-dimensional face
+  barycenter_cart /= 2.; // scale
+  barycenter = tr.barycenter(s, 2);
+  for (std::size_t i = 0; i < barycenter.size(); i++)
+    GUDHI_TEST_FLOAT_EQUALITY_CHECK(barycenter(i), barycenter_cart(i), 1e-7);
   
-  // tr.change_matrix(new_matrix);
-  // tr.change_offset(new_offset);
-  // std::cout << "Triangulation's new matrix is\n" << tr.matrix() << "\n";
-  // std::cout << "Triangulation's new offset is\n" << tr.offset() << "\n";
-
-  // std::cout << "The simplex containing point is " << tr.locate_point(point) << "\n";
-  // std::cout << "The barycenter of the simplex containing point is:\n"
-  // 	    << tr.barycenter(tr.locate_point(point)) << "\n";
-  // std::cout << "The simplex containing point (with scaling) is " << tr.locate_point(point, 2) << "\n";
-  // std::cout << "The barycenter of the simplex containing point (with scaling) is:\n"
-  // 	    << tr.barycenter(tr.locate_point(point, 2), 2) << "\n";
-
-  // Gudhi::coxeter_triangulation::Coxeter_triangulation<> cox_tr(3);
-  // std::cout << "Coxeter triangulation's matrix is\n" << cox_tr.matrix() << "\n";
-  // std::cout << "Coxeter triangulation's offset is\n" << cox_tr.offset() << "\n";
-
-  // {
-  //   std::size_t d = atoi(argv[1]);
-  //   double level = atof(argv[2]);
-  //   Gudhi::coxeter_triangulation::Coxeter_triangulation<> cox_tr(d);
-  //   cox_tr.change_matrix(level * cox_tr.matrix());
-  //   cox_tr.change_offset(Eigen::VectorXd::Random(d));
-  //   double diam = 0;
-  //   auto s = cox_tr.locate_point(Eigen::VectorXd::Zero(d));
-  //   for (auto v1: s.vertex_range())
-  //     for (auto v2: s.vertex_range()) {
-  // 	Eigen::VectorXd p1 = cox_tr.cartesian_coordinates(v1);
-  // 	Eigen::VectorXd p2 = cox_tr.cartesian_coordinates(v2);
-  // 	double dist = (p1-p2).norm();
-  // 	if (dist > diam)
-  // 	  diam = dist;
-  //     }
-  //   std::cout << "The diameter of a simplex in a " << d << "-dimensional Coxeter triangulation with level " << level << " = " << diam << "\n";
-  // }
+  // Matrix and offset change check
+  Eigen::MatrixXd new_matrix(3,3);
+  new_matrix << 1, 0, 0, -1, 1, 0, -1, 0, 1;
+  Eigen::Vector3d new_offset(1.5, 1, 0.5);
+  tr.change_matrix(new_matrix);
+  tr.change_offset(new_offset);
+  
+  BOOST_CHECK( tr.matrix() == new_matrix );
+  BOOST_CHECK( tr.offset() == new_offset );
 }
