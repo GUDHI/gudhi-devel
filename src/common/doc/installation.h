7f426f5a85051676ffb6f8952689bd400ddcc10b
Vincent Rouvreau <10407034+VincentRouvreau@users.noreply.github.com>
2019-12-12 09:40:35 +0100

    Code review: Typo in src/common/doc/installation.h
    Co-Authored-By: Marc Glisse <marc.glisse@inria.fr>

--- src/common/doc/installation.h
+++ src/common/doc/installation.h
@@ -34,7 +34,7 @@ make \endverbatim
  * To test your build, run the following command in a terminal:
  * \verbatim make test \endverbatim
  * `make test` is using <a href="https://cmake.org/cmake/help/latest/manual/ctest.1.html">Ctest<\a> (CMake test driver
- * program). If some of the tests are failing, plend send us the result of the following command:
+ * program). If some of the tests are failing, please send us the result of the following command:
  * \verbatim ctest --output-on-failure \endverbatim
  * 
  * \subsection documentationgeneration Documentation
