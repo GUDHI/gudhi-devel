f3dc8e802d2a6226532f92a252f96ddbd7b6a411
tlacombe <lacombe1993@gmail.com>
2020-01-08 09:41:31 +0100

    changing variable name in test files

--- src/python/test/test_wasserstein_distance.py
+++ src/python/test/test_wasserstein_distance.py
@@ -23,26 +23,26 @@ def test_basic_wasserstein():
     diag4 = np.array([[0, 3], [4, 8]])
     emptydiag = np.array([[]])
 
-    assert wasserstein_distance(emptydiag, emptydiag, q=2., p=1.) == 0.
-    assert wasserstein_distance(emptydiag, emptydiag, q=np.inf, p=1.) == 0.
-    assert wasserstein_distance(emptydiag, emptydiag, q=np.inf, p=2.) == 0.
-    assert wasserstein_distance(emptydiag, emptydiag, q=2., p=2.) == 0.
+    assert wasserstein_distance(emptydiag, emptydiag, internal_p=2., q=1.) == 0.
+    assert wasserstein_distance(emptydiag, emptydiag, internal_p=np.inf, q=1.) == 0.
+    assert wasserstein_distance(emptydiag, emptydiag, internal_p=np.inf, q=2.) == 0.
+    assert wasserstein_distance(emptydiag, emptydiag, internal_p=2., q=2.) == 0.
 
-    assert wasserstein_distance(diag3, emptydiag, q=np.inf, p=1.) == 2.
-    assert wasserstein_distance(diag3, emptydiag, q=1., p=1.) == 4.
+    assert wasserstein_distance(diag3, emptydiag, internal_p=np.inf, q=1.) == 2.
+    assert wasserstein_distance(diag3, emptydiag, internal_p=1., q=1.) == 4.
 
-    assert wasserstein_distance(diag4, emptydiag, q=1., p=2.) == 5.  # thank you Pythagorician triplets
-    assert wasserstein_distance(diag4, emptydiag, q=np.inf, p=2.) == 2.5
-    assert wasserstein_distance(diag4, emptydiag, q=2., p=2.) == 3.5355339059327378
+    assert wasserstein_distance(diag4, emptydiag, internal_p=1., q=2.) == 5.  # thank you Pythagorician triplets
+    assert wasserstein_distance(diag4, emptydiag, internal_p=np.inf, q=2.) == 2.5
+    assert wasserstein_distance(diag4, emptydiag, internal_p=2., q=2.) == 3.5355339059327378
 
-    assert wasserstein_distance(diag1, diag2, q=2., p=1.) == 1.4453593023967701
-    assert wasserstein_distance(diag1, diag2, q=2.35, p=1.74) == 0.9772734057168739
+    assert wasserstein_distance(diag1, diag2, internal_p=2., q=1.) == 1.4453593023967701
+    assert wasserstein_distance(diag1, diag2, internal_p=2.35, q=1.74) == 0.9772734057168739
 
-    assert wasserstein_distance(diag1, emptydiag, q=2.35, p=1.7863) == 3.141592214572228
+    assert wasserstein_distance(diag1, emptydiag, internal_p=2.35, q=1.7863) == 3.141592214572228
 
-    assert wasserstein_distance(diag3, diag4, q=1., p=1.) == 3.
-    assert wasserstein_distance(diag3, diag4, q=np.inf, p=1.) == 3.  # no diag matching here
-    assert wasserstein_distance(diag3, diag4, q=np.inf, p=2.) == np.sqrt(5)
-    assert wasserstein_distance(diag3, diag4, q=1., p=2.) == np.sqrt(5)
-    assert wasserstein_distance(diag3, diag4, q=4.5, p=2.) == np.sqrt(5)
+    assert wasserstein_distance(diag3, diag4, internal_p=1., q=1.) == 3.
+    assert wasserstein_distance(diag3, diag4, internal_p=np.inf, q=1.) == 3.  # no diag matching here
+    assert wasserstein_distance(diag3, diag4, internal_p=np.inf, q=2.) == np.sqrt(5)
+    assert wasserstein_distance(diag3, diag4, internal_p=1., q=2.) == np.sqrt(5)
+    assert wasserstein_distance(diag3, diag4, internal_p=4.5, q=2.) == np.sqrt(5)
 
