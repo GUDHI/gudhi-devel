835acb6cde1b90297908123890f4991da0317301
ROUVREAU Vincent <vincent.rouvreau@inria.fr>
2020-03-02 10:43:24 +0100

    let's try again

--- .github/vcpkg-build-requirements.bat
+++ .github/vcpkg-build-requirements.bat
@@ -1,5 +1,4 @@
 git clone https://github.com/Microsoft/vcpkg
 vcpkg\bootstrap-vcpkg.bat
-vcpkg\vcpkg integrate install
-dir .github
-for /F "delims=" %%a in (.github/vcpkg-requirements.txt) do ( vcpkg\vcpkg install %%a )
+dir
+for /F "delims=" %%a in (vcpkg-requirements.txt) do ( vcpkg\vcpkg install %%a )
