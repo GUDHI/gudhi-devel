7ddad8220fdd34fd3ed91e16882feaa3961b2d67
Marc Glisse <marc.glisse@inria.fr>
2020-03-26 22:59:20 +0100

    license

--- src/python/gudhi/point_cloud/dtm.py
+++ src/python/gudhi/point_cloud/dtm.py
@@ -1,3 +1,12 @@
+# This file is part of the Gudhi Library - https://gudhi.inria.fr/ - which is released under MIT.
+# See file LICENSE or go to https://gudhi.inria.fr/licensing/ for full license details.
+# Author(s):       Marc Glisse
+#
+# Copyright (C) 2020 Inria
+#
+# Modification(s):
+#   - YYYY/MM Author: Description of the modification
+
 from .knn import KNN
 
 
