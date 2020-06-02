25176e3ba75005f36cfe3ebc15e1fcba8cd3227b
Marc Glisse <marc.glisse@inria.fr>
2020-06-02 21:58:48 +0200

    Support e=None for bottleneck_distance

--- src/python/gudhi/bottleneck.cc
+++ src/python/gudhi/bottleneck.cc
@@ -12,22 +12,26 @@
 
 #include <pybind11_diagram_utils.h>
 
-double bottleneck(Dgm d1, Dgm d2, double epsilon)
+// For compatibility with older versions, we want to support e=None.
+// In C++17, the recommended way is std::optional<double>.
+double bottleneck(Dgm d1, Dgm d2, py::object epsilon)
 {
+  double e = (std::numeric_limits<double>::min)();
+  if (!epsilon.is_none()) e = epsilon.cast<double>();
   // I *think* the call to request() has to be before releasing the GIL.
   auto diag1 = numpy_to_range_of_pairs(d1);
   auto diag2 = numpy_to_range_of_pairs(d2);
 
   py::gil_scoped_release release;
 
-  return Gudhi::persistence_diagram::bottleneck_distance(diag1, diag2, epsilon);
+  return Gudhi::persistence_diagram::bottleneck_distance(diag1, diag2, e);
 }
 
 PYBIND11_MODULE(bottleneck, m) {
       m.attr("__license__") = "GPL v3";
       m.def("bottleneck_distance", &bottleneck,
           py::arg("diagram_1"), py::arg("diagram_2"),
-          py::arg("e") = (std::numeric_limits<double>::min)(),
+          py::arg("e") = py::none(),
           R"pbdoc(
     This function returns the point corresponding to a given vertex.
 
