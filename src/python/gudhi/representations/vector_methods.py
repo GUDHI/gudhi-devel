a22b48e00ca858c0e9c300cee87f265d70aeecc7
martinroyer <16647869+martinroyer@users.noreply.github.com>
2020-06-09 12:23:20 +0200

    remove randomness in example

--- src/python/gudhi/representations/vector_methods.py
+++ src/python/gudhi/representations/vector_methods.py
@@ -613,7 +613,7 @@ class Atol(BaseEstimator, TransformerMixin):
     >>> a = np.array([[1, 2, 4], [1, 4, 0], [1, 0, 4]])
     >>> b = np.array([[4, 2, 0], [4, 4, 0], [4, 0, 2]])
     >>> c = np.array([[3, 2, -1], [1, 2, -1]])
-    >>> atol_vectoriser = Atol(quantiser=KMeans(n_clusters=2))
+    >>> atol_vectoriser = Atol(quantiser=KMeans(n_clusters=2, random_state=202006))
     >>> atol_vectoriser.fit(X=[a, b, c]).centers
     array([[ 2.6       ,  2.8       , -0.4       ],
            [ 2.        ,  0.66666667,  3.33333333]])
