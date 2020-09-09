c09f54487cc88eeb3afe1629ff8a3edc223df1eb
ROUVREAU Vincent <vincent.rouvreau@inria.fr>
2020-09-09 11:17:02 +0200

    Even if CGAL was requiring boost::thread no need to find it for gudhi. boost::thread removed from cgal on https://github.com/CGAL/cgal/commit/8a7d3fa

--- src/cmake/modules/GUDHI_third_party_libraries.cmake
+++ src/cmake/modules/GUDHI_third_party_libraries.cmake
@@ -1,6 +1,6 @@
 # This files manage third party libraries required by GUDHI
 
-find_package(Boost 1.56.0 QUIET OPTIONAL_COMPONENTS system thread filesystem unit_test_framework program_options)
+find_package(Boost 1.56.0 QUIET OPTIONAL_COMPONENTS filesystem unit_test_framework program_options)
 
 # Boost_FOUND is not reliable
 if(NOT Boost_VERSION)
