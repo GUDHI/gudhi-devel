710c09b99c3504919625513b8d22ec5436aef7fa
MathieuCarriere <mathieu.carriere3@gmail.com>
2020-10-16 18:19:09 +0200

    add different representations

--- src/python/gudhi/representations/perslay.py
+++ src/python/gudhi/representations/perslay.py
@@ -3,6 +3,79 @@ import tensorflow_addons as tfa
 from tensorflow import random_uniform_initializer as rui
 import numpy as np
 
+def permutation_equivariant_layer(inp, dimension, perm_op, lbda, b, gamma):
+    """ DeepSet PersLay """
+    dimension_before, num_pts = inp.shape[2], inp.shape[1]
+    b = tf.expand_dims(tf.expand_dims(b, 0), 0)
+    A = tf.reshape(tf.einsum("ijk,kl->ijl", inp, lbda), [-1, num_pts, dimension])
+    if perm_op != None:
+        if perm_op == "max":
+            beta = tf.tile(tf.expand_dims(tf.math.reduce_max(inp, axis=1), 1), [1, num_pts, 1])
+        elif perm_op == "min":
+            beta = tf.tile(tf.expand_dims(tf.math.reduce_min(inp, axis=1), 1), [1, num_pts, 1])
+        elif perm_op == "sum":
+            beta = tf.tile(tf.expand_dims(tf.math.reduce_sum(inp, axis=1), 1), [1, num_pts, 1])
+        else:
+            raise Exception("perm_op should be min, max or sum")
+        B = tf.reshape(tf.einsum("ijk,kl->ijl", beta, gamma), [-1, num_pts, dimension])
+        return A - B + b
+    else:
+        return A + b
+
+def rational_hat_layer(inp, q, mu, r):
+    """ Rational Hat PersLay """
+    mu, r = tf.expand_dims(tf.expand_dims(mu, 0), 0), tf.expand_dims(tf.expand_dims(r, 0), 0)
+    dimension_before, num_pts = inp.shape[2], inp.shape[1]
+    bc_inp = tf.expand_dims(inp, -1)
+    norms = tf.norm(bc_inp - mu, ord=q, axis=2)
+    return 1/(1 + norms) - 1/(1 + tf.math.abs(tf.math.abs(r)-norms)) 
+
+def rational_layer(inp, mu, sg, al):
+    """ Rational PersLay """
+    mu, sg, al = tf.expand_dims(tf.expand_dims(mu, 0), 0), tf.expand_dims(tf.expand_dims(sg, 0), 0), tf.expand_dims(tf.expand_dims(al, 0), 0)
+    dimension_before, num_pts = inp.shape[2], inp.shape[1]
+    bc_inp = tf.expand_dims(inp, -1)
+    return 1/tf.math.pow(1+tf.math.reduce_sum(tf.math.multiply(tf.math.abs(bc_inp - mu), tf.math.abs(sg)), axis=2), al)
+
+def exponential_layer(inp, mu, sg):
+    """ Exponential PersLay """
+    mu, sg = tf.expand_dims(tf.expand_dims(mu, 0), 0), tf.expand_dims(tf.expand_dims(sg, 0), 0)
+    dimension_before, num_pts = inp.shape[2], inp.shape[1]
+    bc_inp = tf.expand_dims(inp, -1)
+    return tf.math.exp(tf.math.reduce_sum(-tf.math.multiply(tf.math.square(bc_inp - mu), tf.math.square(sg)), axis=2))
+
+def landscape_layer(inp, sp):
+    """ Landscape PersLay """
+    sp = tf.expand_dims(tf.expand_dims(sp, 0), 0)
+    return tf.math.maximum( .5 * (inp[:, :, 1:2] - inp[:, :, 0:1]) - tf.math.abs(sp - .5 * (inp[:, :, 1:2] + inp[:, :, 0:1])), np.array([0]))
+
+def betti_layer(inp, theta, sp):
+    """ Betti PersLay """
+    sp = tf.expand_dims(tf.expand_dims(sp, 0), 0)
+    X, Y = inp[:, :, 0:1], inp[:, :, 1:2]
+    return  1. / ( 1. + tf.math.exp( -theta * (.5*(Y-X) - tf.math.abs(sp - .5*(Y+X))) )  )
+
+def entropy_layer(inp, theta, sp):
+    """ Entropy PersLay
+    WARNING: this function assumes that padding values are zero
+    """
+    sp = tf.expand_dims(tf.expand_dims(sp, 0), 0)
+    bp_inp = tf.einsum("ijk,kl->ijl", inp, tf.constant(np.array([[1.,-1.],[0.,1.]], dtype=np.float32)))
+    L, X, Y = bp_inp[:, :, 1:2], bp_inp[:, :, 0:1], bp_inp[:, :, 0:1] + bp_inp[:, :, 1:2]
+    LN = tf.math.multiply(L, 1. / tf.expand_dims(tf.linalg.matmul(L[:,:,0], tf.ones([L.shape[1],1])), -1))
+    entropy_terms = tf.where(LN > 0., -tf.math.multiply(LN, tf.math.log(LN)), LN)
+    return  tf.math.multiply(entropy_terms, 1. / ( 1. + tf.math.exp( -theta * (.5*(Y-X) - tf.math.abs(sp - .5*(Y+X))) )  ))
+
+def image_layer(inp, image_size, image_bnds, sg):
+    """ Persistence Image PersLay """
+    bp_inp = tf.einsum("ijk,kl->ijl", inp, tf.constant(np.array([[1.,-1.],[0.,1.]], dtype=np.float32)))
+    dimension_before, num_pts = inp.shape[2], inp.shape[1]
+    coords = [tf.range(start=image_bnds[i][0], limit=image_bnds[i][1], delta=(image_bnds[i][1] - image_bnds[i][0]) / image_size[i]) for i in range(dimension_before)]
+    M = tf.meshgrid(*coords)
+    mu = tf.concat([tf.expand_dims(tens, 0) for tens in M], axis=0)
+    bc_inp = tf.reshape(bp_inp, [-1, num_pts, dimension_before] + [1 for _ in range(dimension_before)])
+    return tf.expand_dims(tf.math.exp(tf.math.reduce_sum(  -tf.math.square(bc_inp-mu) / (2*tf.math.square(sg)),  axis=2)) / (2*np.pi*tf.math.square(sg)), -1)
+
 class PerslayModel(tf.keras.Model):
 
     def __init__(self, name, diagdim, perslay_parameters, rho):
