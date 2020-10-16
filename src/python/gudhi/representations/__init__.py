b33ce0631f8a1474c17029764af1c4fd35578f05
MathieuCarriere <mathieu.carriere3@gmail.com>
2020-10-16 18:14:14 +0200

    modify __init__

--- src/python/gudhi/representations/__init__.py
+++ src/python/gudhi/representations/__init__.py
@@ -2,5 +2,6 @@ from .kernel_methods import *
 from .metrics import *
 from .preprocessing import *
 from .vector_methods import *
+from .perslay import *
 
-__all__ = ["kernel_methods", "metrics", "preprocessing", "vector_methods"]
+__all__ = ["kernel_methods", "metrics", "preprocessing", "vector_methods", "perslay"]
