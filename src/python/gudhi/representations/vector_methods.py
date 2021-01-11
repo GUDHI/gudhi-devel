2a29df7cd54e9689e93bab90e3f64c84e8e8790f
ROUVREAU Vincent <vincent.rouvreau@inria.fr>
2021-01-11 13:59:01 +0100

    skip doctest (but run them)

--- src/python/gudhi/representations/vector_methods.py
+++ src/python/gudhi/representations/vector_methods.py
@@ -605,14 +605,14 @@ class Atol(BaseEstimator, TransformerMixin):
     >>> b = np.array([[4, 2, 0], [4, 4, 0], [4, 0, 2]])
     >>> c = np.array([[3, 2, -1], [1, 2, -1]])
     >>> atol_vectoriser = Atol(quantiser=KMeans(n_clusters=2, random_state=202006))
-    >>> atol_vectoriser.fit(X=[a, b, c]).centers
+    >>> atol_vectoriser.fit(X=[a, b, c]).centers #doctest: +SKIP
     >>> # array([[ 2.        ,  0.66666667,  3.33333333],
     >>> #        [ 2.6       ,  2.8       , -0.4       ]])
     >>> atol_vectoriser(a)
-    >>> # array([1.18168665, 0.42375966])
+    >>> # array([1.18168665, 0.42375966]) #doctest: +SKIP
     >>> atol_vectoriser(c)
-    >>> # array([0.02062512, 1.25157463])
-    >>> atol_vectoriser.transform(X=[a, b, c])
+    >>> # array([0.02062512, 1.25157463]) #doctest: +SKIP
+    >>> atol_vectoriser.transform(X=[a, b, c]) #doctest: +SKIP
     >>> # array([[1.18168665, 0.42375966],
     >>> #        [0.29861028, 1.06330156],
     >>> #        [0.02062512, 1.25157463]])
