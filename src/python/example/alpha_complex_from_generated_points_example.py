7573e67c8c6c1bb3cd21fd8b9ffb8aa0168eb7f7
Hind <hind.montassif@gmail.com>
2021-04-27 15:13:25 +0200

    Remove the commented graphic part from the example (to be added to tutorial notebooks)

--- src/python/example/alpha_complex_from_generated_points_example.py
+++ src/python/example/alpha_complex_from_generated_points_example.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 
 from gudhi import random_point_generators
-from gudhi import AlphaComplex, SimplexTree
-from gudhi import plot_persistence_barcode, plot_persistence_diagram
-
-import matplotlib.pyplot as plt
+from gudhi import AlphaComplex
 
 
 """ This file is part of the Gudhi Library - https://gudhi.inria.fr/ - which is released under MIT.
@@ -28,10 +25,6 @@ print("AlphaComplex creation from generated points")
 # Generate a circle: 50 points; dim 2; radius 1
 points = random_point_generators.generate_points_on_sphere_d(50, 2, 1)
 
-# Plot the generated points (to uncomment if wished)
-#plt.scatter(points[:,0], points[:,1])
-#plt.show()
-
 # Create an alpha complex
 alpha_complex = AlphaComplex(points=points)
 simplex_tree = alpha_complex.create_simplex_tree()
@@ -41,12 +34,3 @@ result_str = 'Alpha complex is of dimension ' + repr(simplex_tree.dimension()) +
     repr(simplex_tree.num_vertices()) + ' vertices.'
 print(result_str)
 
-
-# Compute the persistence
-diag = simplex_tree.persistence()
-
-# Plot the barcode and diagram (to uncomment if wished)
-#plot_persistence_barcode(diag)
-#plt.show()
-#plot_persistence_diagram(diag)
-#plt.show()
