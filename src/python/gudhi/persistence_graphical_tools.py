87da488d24c70cbd470ad1c2dae762af68cd227e
ROUVREAU Vincent <vincent.rouvreau@inria.fr>
2021-08-24 14:50:12 +0200

    code review: isinstance can take several instances types as a list

--- src/python/gudhi/persistence_graphical_tools.py
+++ src/python/gudhi/persistence_graphical_tools.py
@@ -60,7 +60,7 @@ def _array_handler(a):
                 persistence-compatible list (padding with 0), so that the
                 plot can be performed seamlessly.
     """
-    if isinstance(a[0][1], np.floating) or isinstance(a[0][1], float):
+    if isinstance(a[0][1], (np.floating, float)):
         return [[0, x] for x in a]
     else:
         return a
