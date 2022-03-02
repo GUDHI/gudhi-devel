8e5003f37d843d92b8fd6f59e275ac0d3ae607e5
Felix Hensel <hensel.felix@gmail.com>
2022-03-02 11:28:18 +0100

    updated test

--- src/python/test/test_ripsnet.py
+++ src/python/test/test_ripsnet.py
@@ -10,39 +10,43 @@ import numpy             as np
 import tensorflow        as tf
 from gudhi.tensorflow.ripsnet import *
 
+
 def test_ripsnet():
-    input_dim = 2
-    ragged_layers_size = [10]
-    dense_layers_size = [10]
-    output_units = 2
+    ragged_layers_size = [4]
+    dense_layers_size = [4]
+    output_units = 4
     activation_fct = 'gelu'
     output_activation = 'sigmoid'
     dropout = 0
     kernel_regularization = 0
+    initializer = None# tf.keras.initializers.Constant(value=1)
 
     ragged_layers = []
     dense_layers = []
     for n_units in ragged_layers_size:
-        ragged_layers.append(DenseRagged(units=n_units, use_bias=True, activation=activation_fct))
+        ragged_layers.append(DenseRagged(units=n_units, use_bias=True, activation=activation_fct,
+                                         kernel_initializer=initializer, bias_initializer=initializer))
 
     for n_units in dense_layers_size:
-        dense_layers.append(tf.keras.layers.Dense(n_units, activation=activation_fct))
+        dense_layers.append(tf.keras.layers.Dense(n_units, activation=activation_fct,
+                                                  kernel_initializer=initializer, bias_initializer=initializer))
 
-    dense_layers.append(tf.keras.layers.Dense(output_units, activation=output_activation))
+    dense_layers.append(tf.keras.layers.Dense(output_units, activation=output_activation,
+                                              kernel_initializer=initializer, bias_initializer=initializer))
 
-    weights_vect = [np.array([[-0.82675004, 0.082853, -0.7522571, -0.86455333],
-                              [0.01191106, 0.4168273, 0.13942415, -0.95728004]]),
-                    np.array([-0.48964924, 0.07440309, -0.84914917, -0.13673241]),
-                    np.array([[0.6735323, -0.86710656, -0.6601294, 0.7874811],
-                              [-0.5002684, -0.36409453, 0.5563092, -0.16215993],
-                              [0.31057122, -0.79817116, -0.40550056, 0.8173016],
-                              [0.64593965, 0.5079483, -0.5176215, 0.5098056]]),
-                    np.array([0.68127185, -0.03681624, -0.46403527, 0.42004192]),
-                    np.array([[-0.23722765, 0.18152483, 0.42457554, 0.55383813],
-                              [0.11030384, -0.72890997, 0.81839615, -0.26585487],
-                              [-0.16284865, -0.22054555, -0.03428887, -0.10710541],
-                              [-0.6388146, -0.6412578, -0.05625373, 0.6325129]]),
-                    np.array([0.31346196, 0.32782924, 0.18332976, -0.4718462])]
+    weights_vect = [np.array([[-0.3868327 ,  0.5431584 ,  0.7523476 ,  0.80209386],
+                              [ 0.22491306,  0.4626178 ,  0.34193814, -0.04737851]]),
+                    np.array([ 0.5047069 , -0.11543324, -0.03882882, -0.16129738]),
+                    np.array([[-0.7956421  ,  0.2326832  , -0.5405302  ,  0.096256964],
+                              [ 0.06973686 ,  0.0251764  , -0.05733281 ,  0.3528394  ],
+                              [-0.77462643 ,  0.03330394 , -0.8688136  , -0.22296508 ],
+                              [-0.5054477  ,  0.7201048  ,  0.1857564  ,  0.65894866 ]]),
+                    np.array([-0.30565566 , -0.77507186 , -0.049963538,  0.5765676  ]),
+                    np.array([[-0.25560755  ,  0.71504813  ,  0.0047909063, -0.1595783   ],
+                              [-0.71575665  ,  0.6139034   , -0.47060093  ,  0.087501734 ],
+                              [ 0.1588738   , -0.593038    ,  0.48378325  , -0.777213    ],
+                              [ 0.6206032   , -0.20880768  ,  0.14528894  ,  0.18696047  ]]),
+                    np.array([-0.17761804, -0.6905532 ,  0.64367545, -0.2173939 ])]
 
     phi_1 = DenseRaggedBlock(ragged_layers)
     perm_op = 'mean'
@@ -51,46 +55,56 @@ def test_ripsnet():
 
     model = RipsNet(phi_1, phi_2, input_dim, perm_op=perm_op)
 
+    test_input_raw = [np.array([[1.,2.],[3.,4.]])]
+
+    test_input = tf.ragged.constant([
+        [list(c) for c in list(test_input_raw[i])] for i in range(len(test_input_raw))], ragged_rank=1)
+
+    model.predict(test_input)
+
     model.set_weights(weights_vect)
 
-    clean_data_test = [np.array([[  8.00690107,   5.84065138],
-                                 [-10.01871508,   5.34973559],
-                                 [ -9.65317915,   5.53776047],
-                                 [  7.78598811,   7.76091515]]),
-                       np.array([[  9.95347807,  13.72474594],
-                                 [ -3.40168368, -15.23953774],
-                                 [ -8.73604688,   6.76905796]]),
-                       np.array([[ 3.25849098, -4.01998912],
-                                 [ 2.23853692, -2.96113938],
-                                 [ 3.38496086, -4.47234084],
-                                 [ 2.13325439, -2.91673025]])]
 
-    noisy_data_test = [np.array([[-15.49749451,  -4.81412853],
-                                 [-10.02162025,   0.42705654],
-                                 [-14.41883655,  10.24555917],
-                                 [ -8.6865668 ,   6.12261606]]),
-                       np.array([[-3.14624955,  2.59008951],
-                                 [-3.88721637,  0.65461877],
-                                 [-3.16411703,  0.44306022],
-                                 [-2.75523434,  1.57533588]]),
-                       np.array([[ 10.14006214,  10.92637587],
-                                 [-13.75312184,  -2.09110089],
-                                 [ -7.83379779,   0.76465675]])]
+    clean_data_test = [np.array([[ -7.04493841,   9.60285858],
+                                 [-13.14389003, -13.21854157],
+                                 [ -3.21137961,  -1.28593644]]),
+                       np.array([[ 10.40324933,  -0.80540584],
+                                 [ 16.54752459,   0.70355361],
+                                 [  6.410207  , -10.63175183],
+                                 [  2.96613799, -11.97463568]]),
+                       np.array([[ 4.85041719, -2.93820024],
+                                 [ 2.15379915, -5.39669696],
+                                 [ 5.83968556, -5.67350982],
+                                 [ 5.25955172, -6.36860269]])]
+
+    noisy_data_test = [np.array([[ -8.93311026,   1.52317533],
+                                 [-16.80344139,  -3.76871298],
+                                 [-11.58448573,  -2.76311122],
+                                 [-15.06107796,   5.05253587]]),
+                       np.array([[-3.834947  , -5.1897498 ],
+                                 [-3.51701182, -4.23539191],
+                                 [-2.68678747, -1.63902703],
+                                 [-4.65070816, -3.96363227]]),
+                       np.array([[ 4.7841113 , 19.2922069 ],
+                                 [10.5164214 ,  5.50246605],
+                                 [-9.38163622,  7.03682948]])]
 
     tf_clean_data_test = tf.ragged.constant([
         [list(c) for c in list(clean_data_test[i])] for i in range(len(clean_data_test))], ragged_rank=1)
     tf_noisy_data_test = tf.ragged.constant([
         [list(c) for c in list(noisy_data_test[i])] for i in range(len(noisy_data_test))], ragged_rank=1)
 
-    clean_prediction = np.array([[0.3942722,  0.95260733, 0.9447353,  0.00872418],
-                        [0.70753145, 0.74972904, 0.45573318, 0.25137717],
-                        [0.6459818,  0.5658171,  0.19202137, 0.6256107 ]])
-    noisy_prediction = np.array([[0.5073361,  0.93115664, 0.89942205, 0.01895535],
-                        [0.4248793,  0.8612615,  0.7404877,  0.09027195],
-                        [0.36387908, 0.9295354,  0.90001035, 0.01976481]])
+    clean_prediction = np.array([[0.5736222, 0.3047213, 0.6746019, 0.49468565],
+                                 [0.4522748, 0.7521156, 0.3061385, 0.77519494],
+                                 [0.5349713, 0.49940312, 0.51753736, 0.6435147]])
+    noisy_prediction = np.array([[0.53986603, 0.33934325, 0.64809155, 0.49939266],
+                                 [0.5637899, 0.28744557, 0.67097586, 0.50240993],
+                                 [0.5689339,  0.55574864, 0.47079712, 0.68721807]])
+
+    #print(np.linalg.norm(clean_prediction - model.predict(tf_clean_data_test)))
 
     assert(clean_prediction.shape == model.predict(tf_clean_data_test).shape)
     assert(noisy_prediction.shape == model.predict(tf_noisy_data_test).shape)
-    assert((clean_prediction == model.predict(tf_clean_data_test)).all())
-    assert((noisy_prediction == model.predict(tf_noisy_data_test)).all())
+    assert(np.linalg.norm(clean_prediction - model.predict(tf_clean_data_test)) <= 1e-7)
+    assert(np.linalg.norm(noisy_prediction - model.predict(tf_noisy_data_test)) <= 1e-7)
     return
