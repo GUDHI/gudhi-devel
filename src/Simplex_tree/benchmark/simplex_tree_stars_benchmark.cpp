468db5e60c13c820d9e71f3dc68e290f502ac105
Vincent Rouvreau <vincent.rouvreau@inria.fr>
2023-06-05 15:27:42 +0200

    code review: quite better to use std::sample to pick 20.000 random simplices from the Simplex_tree

--- src/Simplex_tree/benchmark/simplex_tree_stars_benchmark.cpp
+++ src/Simplex_tree/benchmark/simplex_tree_stars_benchmark.cpp
@@ -14,7 +14,7 @@
 #include <iostream>
 #include <random>
 #include <numeric>  // for std::iota
-#include <algorithm>  // for std::shuffle
+#include <algorithm>  // for std::shuffle and std::sample
 #include <vector>
 
 std::random_device rd;
@@ -50,31 +50,26 @@ void benchmark_stars_computation() {
   std::clog << "... Simplex_tree construction" << std::endl;
 
   Stree st;
-  // Insert 5000 random simplices, of size in between [2; 5] and vertices in between [0; 5000]
+  // Insert 5.000 random simplices, of size in between [2; 5] and vertices in between [0; 5.000]
   for (Vertex_handle v=0; v < 5000; v++)
     st.insert_simplex_and_subfaces(rand_int_range<Vertex_handle>(2, 5, 5000));
   std::cout << "... " << st.num_vertices() << " vertices and " << st.num_simplices() << " simplices." << std::endl;
   
   Gudhi::Clock benchmark_search("... Looking for random existing simplices");
-  const int SH_SIZE = 10000;
+  // 5.000 random simplices of size in between [2; 5] has been inserted - about 50.000 simplices in the Simplex_tree
+  const int SH_SIZE = 20000;
   std::vector<Simplex_handle> sh_list;
   sh_list.reserve(SH_SIZE);
 
-  std::uniform_int_distribution<int> dist(0, st.num_simplices());
-  // This method is not equivalent but quite faster than iterating randomly on complex_simplex_range
-  while (sh_list.size() < SH_SIZE) {
-    auto simplex = st.complex_simplex_range().begin();
-    // Looking for a random simplex - No operator+ for Complex_simplex_range, loop and increment is required
-    for (int nb_rand = dist(rd); nb_rand > 0; nb_rand--)
-      simplex++;
-    sh_list.push_back(*simplex);
-  }
+  std::sample(st.complex_simplex_range().begin(), st.complex_simplex_range().end(), std::back_inserter(sh_list),
+              SH_SIZE, std::mt19937 {rd()});
   std::clog << benchmark_search << std::endl;
 
   Gudhi::Clock benchmark_stars("Benchmark the stars search of the random simplices");
-  // Just browse the stars from the list
+  // Just browse the stars from the random simplices list
   for (auto& sh : sh_list)
-    for ([[maybe_unused]] const auto& simplex : st.star_simplex_range(sh)) { [[maybe_unused]] volatile auto _ = simplex; }
+    for ([[maybe_unused]] const auto& simplex : st.star_simplex_range(sh))
+      [[maybe_unused]] volatile auto _ = simplex;
 
   std::clog << benchmark_stars << std::endl;
 }
