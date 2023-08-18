b29002a416a6a53f064925ca7fd3ccb809d4234f
Vincent Rouvreau <vincent.rouvreau@inria.fr>
2023-08-18 16:12:12 +0200

    codespell: siz -> graph_size

--- src/Bottleneck_distance/include/gudhi/Bottleneck.h
+++ src/Bottleneck_distance/include/gudhi/Bottleneck.h
@@ -36,11 +36,11 @@ namespace persistence_diagram {
 inline double bottleneck_distance_approx(Persistence_graph& g, double e) {
   double b_lower_bound = 0.;
   double b_upper_bound = g.max_dist_to_diagonal();
-  int siz = g.size();
-  if (siz <= 1)
+  int graph_size = g.size();
+  if (graph_size <= 1)
     // The value of alpha would be wrong in this case
     return b_upper_bound;
-  const double alpha = std::pow(siz, 1. / 5.);
+  const double alpha = std::pow(graph_size, 1. / 5.);
   Graph_matching m(g);
   Graph_matching biggest_unperfect(g);
   while (b_upper_bound - b_lower_bound > 2 * e) {
