7b5ffc5ec03afc5b4300badc6060daab450c195b
MathieuCarriere <mathieu.carriere3@gmail.com>
2023-11-10 11:52:01 +0100

    fix dependency problems

--- src/Nerve_GIC/utilities/KeplerMapperVisuFromTxtFile.py
+++ src/Nerve_GIC/utilities/KeplerMapperVisuFromTxtFile.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 
-import km
-import numpy as np
-from collections import defaultdict
 import argparse
+from gudhi.cover_complex import save_to_html_
 
 """This file is part of the Gudhi Library - https://gudhi.inria.fr/ - which is released under MIT.
     See file LICENSE or go to https://gudhi.inria.fr/licensing/ for full license details.
@@ -18,45 +16,6 @@ import argparse
 __author__ = "Mathieu Carriere"
 __copyright__ = "Copyright (C) 2017 Inria"
 __license__ = "GPL v3"
-
-def save_to_html_(dat, lens, color, param, nums, points, edges, html_output_filename):
-
-    network = {}
-    mapper = km.KeplerMapper(verbose=0)
-    data = np.zeros((3,3))
-    projected_data = mapper.fit_transform( data, projection="sum", scaler=None )
-
-    num_nodes = nums[0]
-    num_edges = nums[1]
-
-    nodes = defaultdict(list)
-    links = defaultdict(list)
-    custom = defaultdict(list)
-
-    for point in points:
-        nodes[  str(int(point[0]))  ] = [  int(point[0]), point[1], int(point[2])  ]
-        links[  str(int(point[0]))  ] = []
-        custom[  int(point[0])  ] = point[1]
-
-    for edge in edges:
-        links[  str(edge[0])  ].append(  str(edge[1])  )
-        links[  str(edge[1])  ].append(  str(edge[0])  )
-
-    m = min([custom[i] for i in range(0,num_nodes)])
-    M = max([custom[i] for i in range(0,num_nodes)])
-
-    network["nodes"] = nodes
-    network["links"] = links
-    network["meta"] = lens
-
-
-    mapper.visualize(network, color_function=color, path_html=html_output_filename, title=dat,
-    graph_link_distance=30, graph_gravity=0.1, graph_charge=-120, custom_tooltips=custom, width_html=0,
-    height_html=0, show_tooltips=True, show_title=True, show_meta=True, res=param[0], gain=param[1], minimum=m, maximum=M)
-    message = repr(html_output_filename) + " is generated. You can now use your favorite web browser to visualize it."
-    print(message)
-
-
 
 parser = argparse.ArgumentParser(description='Creates an html Keppler Mapper '
                                  'file to visualize a SC.txt file.',
--- src/python/gudhi/cover_complex.py
+++ src/python/gudhi/cover_complex.py
@@ -18,9 +18,46 @@ from sklearn.metrics         import pairwise_distances
 from scipy.spatial.distance  import directed_hausdorff
 
 from . import SimplexTree, CoverComplex
-import sys
-sys.path.append("../../Nerve_GIC/utilities/")
-from KeplerMapperVisuFromTxtFile import save_to_html_
+
+def save_to_html_(dat, lens, color, param, nums, points, edges, html_output_filename):
+
+    from .kepler_mapper import KeplerMapper
+    network = {}
+    mapper = KeplerMapper(verbose=0)
+    data = np.zeros((3,3))
+    projected_data = mapper.fit_transform( data, projection="sum", scaler=None )
+
+    num_nodes = nums[0]
+    num_edges = nums[1]
+
+    from collections import defaultdict
+    nodes = defaultdict(list)
+    links = defaultdict(list)
+    custom = defaultdict(list)
+
+    for point in points:
+        nodes[  str(int(point[0]))  ] = [  int(point[0]), point[1], int(point[2])  ]
+        links[  str(int(point[0]))  ] = []
+        custom[  int(point[0])  ] = point[1]
+
+    for edge in edges:
+        links[  str(edge[0])  ].append(  str(edge[1])  )
+        links[  str(edge[1])  ].append(  str(edge[0])  )
+
+    m = min([custom[i] for i in range(0,num_nodes)])
+    M = max([custom[i] for i in range(0,num_nodes)])
+
+    network["nodes"] = nodes
+    network["links"] = links
+    network["meta"] = lens
+
+
+    mapper.visualize(network, color_function=color, path_html=html_output_filename, title=dat,
+    graph_link_distance=30, graph_gravity=0.1, graph_charge=-120, custom_tooltips=custom, width_html=0,
+    height_html=0, show_tooltips=True, show_title=True, show_meta=True, res=param[0], gain=param[1], minimum=m, maximum=M)
+    message = repr(html_output_filename) + " is generated. You can now use your favorite web browser to visualize it."
+    print(message)
+
 
 class CoverComplexPy(BaseEstimator):
     """
