82201e4e335214da8997b6c07c658999d4dcca5b
Vincent Rouvreau <vincent.rouvreau@inria.fr>
2024-03-11 09:47:13 +0100

    Vectorize random values, but same timings

--- src/common/include/gudhi/random_graph_generators.h
+++ src/common/include/gudhi/random_graph_generators.h
@@ -19,6 +19,7 @@
 #include <array>
 #include <random>
 #include <numeric>  // for std::iota
+#include <cstddef>  // for std::size_t
 #ifdef DEBUG_TRACES
 #include <iostream>
 #endif  // DEBUG_TRACES
@@ -27,22 +28,35 @@ std::random_device rd;
 
 namespace Gudhi {
 
+template <typename Floating_type>
+std::vector<Floating_type> N_random_values(std::size_t nb_values, Floating_type min = 0., Floating_type max = 1.) {
+  std::uniform_real_distribution<Floating_type> unif(0., 1.);
+  std::mt19937 rand_engine(rd());
+  std::vector<Floating_type> random_filtrations(nb_values);
+  std::generate(random_filtrations.begin(), random_filtrations.end(),
+                [&unif, &rand_engine]() { return unif(rand_engine); });
+  return random_filtrations;
+}
+
 template <typename Vertex_handle>
 std::vector<std::array<Vertex_handle, 2>> random_edges(Vertex_handle nb_vertices, double density = 0.15) {
   std::vector<std::array<Vertex_handle, 2>> permutations;
   if (nb_vertices < 2)
     return permutations;
 
-  std::uniform_real_distribution<double> unif(0., 1.);
-  std::mt19937 rand_engine(rd());
-
   std::vector<bool> to_permute(nb_vertices);
   std::fill(to_permute.begin(), to_permute.begin() + 2, true);
   
+  std::size_t nb_permutations = (nb_vertices * (nb_vertices - 1)) / 2;
+  auto random_values = N_random_values<double>(nb_permutations);
+  std::size_t idx = 0;
   do {
     // Keep only X% of the possible edges
-    if (unif(rand_engine) > density)
+    if (random_values[idx] > density) {
+      idx++;
       continue;
+    }
+    idx++;
 
     std::array<Vertex_handle, 2> permutation = {-1, -1};
     for (Vertex_handle idx = 0; idx < nb_vertices; ++idx) {
@@ -76,11 +90,14 @@ void simplex_tree_random_flag_complex(
   std::iota(vertices.begin(), vertices.end(), 0); // vertices is { 0, 1, 2, ..., 99 } when nb_vertices is 100
   st.insert_batch_vertices(vertices);
 
-  std::uniform_real_distribution<double> unif(0., 1.);
-  std::mt19937 rand_engine(rd());
+  auto edges = random_edges(nb_vertices, density);
 
-  for (auto edge : random_edges(nb_vertices, density)) {
-    st.insert_simplex(edge, unif(rand_engine));
+  auto random_filtrations = N_random_values<typename Simplex_tree::Filtration_value>(edges.size());
+
+  std::size_t idx = 0;
+  for (auto edge : edges) {
+    st.insert_simplex(edge, random_filtrations[idx]);
+    idx++;
   }
 
 }
