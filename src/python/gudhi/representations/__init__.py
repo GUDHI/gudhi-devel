5d8af99da302496b3ff860d6a169dea06e99e37d
Martin ROYER <martin.royer@irtsystemx.fr>
2024-06-07 19:56:56 +0200

    remove archipelago

--- src/python/gudhi/representations/__init__.py
+++ src/python/gudhi/representations/__init__.py
@@ -2,6 +2,5 @@ from .kernel_methods import *
 from .metrics import *
 from .preprocessing import *
 from .vector_methods import *
-from .archipelago import *
 
-__all__ = ["kernel_methods", "metrics", "preprocessing", "vector_methods", "archipelago"]
+__all__ = ["kernel_methods", "metrics", "preprocessing", "vector_methods"]
