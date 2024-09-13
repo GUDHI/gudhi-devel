7a282a1ed567f52b855b560550ab2dc86cdafb0c
Marc Glisse <marc.glisse@inria.fr>
2024-09-13 21:49:07 +0200

    reformat test file with black

--- src/python/test/test_sklearn_rips_persistence.py
+++ src/python/test/test_sklearn_rips_persistence.py
@@ -37,12 +37,12 @@ def test_h1_only_rips_persistence_of_points_on_a_circle():
     rips = RipsPersistence(homology_dimensions=1, n_jobs=-2)
     diags = rips.fit_transform([points.sphere(n_samples=150, ambient_dim=2)])[0]
     assert len(diags) == 1
-    assert 0. < diags[0][0] < 0.6
-    assert 1. < diags[0][1] < 2.
+    assert 0.0 < diags[0][0] < 0.6
+    assert 1.0 < diags[0][1] < 2.0
 
 
 def test_invalid_input_type():
-    rips = RipsPersistence(homology_dimensions=0, input_type='whatsoever')
+    rips = RipsPersistence(homology_dimensions=0, input_type="whatsoever")
     with pytest.raises(ValueError):
         rips.fit_transform([points.sphere(n_samples=10, ambient_dim=2)])
 
@@ -50,13 +50,14 @@ def test_invalid_input_type():
 def test_distance_matrix_rips_persistence_of_points_on_a_circle():
     try:
         from scipy.spatial.distance import cdist
+
         pts = points.sphere(n_samples=150, ambient_dim=2)
         distance_matrix = cdist(pts, pts)
-        rips = RipsPersistence(homology_dimensions=1, input_type='lower distance matrix')
+        rips = RipsPersistence(homology_dimensions=1, input_type="lower distance matrix")
         diags = rips.fit_transform([distance_matrix])[0]
         assert len(diags) == 1
-        assert 0. < diags[0][0] < 0.6
-        assert 1. < diags[0][1] < 2.
+        assert 0.0 < diags[0][0] < 0.6
+        assert 1.0 < diags[0][1] < 2.0
     except ValueError:
         pass
 
@@ -64,13 +65,14 @@ def test_distance_matrix_rips_persistence_of_points_on_a_circle():
 def test_set_output():
     try:
         import pandas
+
         NB_PC = 5
         point_clouds = [points.sphere(n_samples=random.randint(100, 150), ambient_dim=2) for _ in range(NB_PC)]
 
         rips = RipsPersistence(homology_dimensions=[0, 2], n_jobs=-2)
         diags_pandas = rips.set_output(transform="pandas").fit_transform(point_clouds)
-        assert 'H0' == diags_pandas.columns[0]
-        assert 'H2' == diags_pandas.columns[1]
+        assert "H0" == diags_pandas.columns[0]
+        assert "H2" == diags_pandas.columns[1]
         assert len(diags_pandas.index) == NB_PC
     except ImportError:
         print("Missing pandas, skipping set_output test")
@@ -78,10 +80,10 @@ def test_set_output():
 
 def test_big():
     # A circle + many isolated points
-    n=1000000
-    X=np.zeros((n,2))
-    X[:,0]=np.arange(n)*100
-    X[:24]=points.torus(24,1,'grid')
+    n = 1000000
+    X = np.zeros((n, 2))
+    X[:, 0] = np.arange(n) * 100
+    X[:24] = points.torus(24, 1, "grid")
     # Ripser cannot handle it, have to fall back to SimplexTree
     # Computing the full distance matrix would require too much memory -> kd-tree
     RipsPersistence(range(25), threshold=10).fit_transform([X])
