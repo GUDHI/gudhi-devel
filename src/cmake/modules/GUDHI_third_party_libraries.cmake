848d853159083041511b070b85389ebb8cb217a6
Vincent Rouvreau <vincent.rouvreau@inria.fr>
2024-09-25 10:03:02 +0200

    Specific case for PyKeops on Windows

--- src/cmake/modules/GUDHI_third_party_libraries.cmake
+++ src/cmake/modules/GUDHI_third_party_libraries.cmake
@@ -179,7 +179,6 @@ if (WITH_GUDHI_PYTHON)
     find_python_module("POT")
     find_python_module("pybind11")
     find_python_module("torch")
-    find_python_module("pykeops")
     find_python_module("eagerpy")
     find_python_module("hnswlib")
     find_python_module("tensorflow")
@@ -187,6 +186,17 @@ if (WITH_GUDHI_PYTHON)
     find_python_module("pydata_sphinx_theme")
     find_python_module("sphinxcontrib.bibtex")
     find_python_module("networkx")
+  endif()
+
+  # Specific case for PyKeops that can be imported on Windows, but fails because it uses fcntl (not available on Windows)
+  # Also fcntl has no metadata, so find_python_module does not work
+  # "import fcntl" is about 1 sec. faster than "import pykeops"
+  execute_process(
+          COMMAND ${Python_EXECUTABLE}  -c "import fcntl"
+          RESULT_VARIABLE FCNTL_IMPORT_MODULE_RESULT
+          OUTPUT_VARIABLE FCNTL_IMPORT_MODULE_OUPUT)
+  if(FCNTL_IMPORT_MODULE_RESULT EQUAL 0)
+    find_python_module("pykeops")
   endif()
 
   if(NOT GUDHI_PYTHON_PATH)
