c5112c0109babde8d106ec086a7bab7fd6d473ee
Erwan Demairy <Erwan.Demairy@inria.fr>
2025-03-28 16:34:28 +0100

    reader_utils wrapped with nanobind.

--- src/python/gudhi/_reader_utils.cc
+++ src/python/gudhi/_reader_utils.cc
@@ -17,6 +17,8 @@
 #include <nanobind/nanobind.h>
 #include <nanobind/stl/string.h>
 #include <nanobind/stl/vector.h>
+#include <nanobind/stl/pair.h>
+#include <nanobind/stl/map.h>
 
 #include "../../Bitmap_cubical_complex/doc/Gudhi_Cubical_Complex_doc.h"
 
@@ -26,6 +28,8 @@ NB_MODULE(_reader_utils_ext, m) {
  m.attr("__license__") = "GPL v3";
 
  m.def("read_matrix_from_csv_file", &Gudhi::read_matrix_from_csv_file, "read_matrix_from_csv_file");
+ m.def("read_pers_intervals_grouped_by_dimension", &Gudhi::read_pers_intervals_grouped_by_dimension, "read_pers_intervals_grouped_by_dimension");
+ m.def("read_pers_intervals_in_dimension", &Gudhi::read_pers_intervals_in_dimension, "read_pers_intervals_in_dimension");
 }
 
 //
