6f7e3ca9c65451b6d1bdb2019fb5e3c6f8944ae0
hschreiber <hannah.schreiber.k@googlemail.com>
2025-04-29 19:50:44 +0200

    truc part2

--- src/Multi_persistence/include/gudhi/Multi_parameter_filtered_complex.h
+++ src/Multi_persistence/include/gudhi/Multi_parameter_filtered_complex.h
@@ -18,11 +18,14 @@
 #ifndef MP_FILTERED_COMPLEX_H_INCLUDED
 #define MP_FILTERED_COMPLEX_H_INCLUDED
 
+#include <algorithm>
 #include <cstdint>  //std::uint32_t
+#include <numeric>
 #include <ostream>
 #include <vector>
 
-// #include <gudhi/Debug_utils.h>
+#include <gudhi/Debug_utils.h>
+#include <gudhi/Multi_parameter_filtration.h>  //for lex order
 
 namespace Gudhi {
 namespace multi_persistence {
@@ -36,53 +39,218 @@ class Multi_parameter_filtered_complex
   using Filtration_value = Multi_filtration_value;
   using T = typename Filtration_value::value_type;
   using Filtration_value_container = std::vector<Filtration_value>;
-  using Boundary_container = std::vector<std::vector<Index> >;
+  using Boundary = std::vector<Index>;
+  using Boundary_container = std::vector<Boundary>;
   using Dimension = int;
+  using Dimension_container = std::vector<Dimension>;
 
-  // has to guarantee sorted by dimension.
-  Multi_parameter_filtered_complex() {}
+  Multi_parameter_filtered_complex()
+      : boundaries_(), dimensions_(), filtrationValues_(), maxDimension_(0), isOrderedByDimension_(true)
+  {}
 
-  Index get_number_of_cycle_generators() const;
-  Index get_number_of_parameters() const;
+  // assumes boundary Idxs corresponds to container Idxs
+  Multi_parameter_filtered_complex(const Boundary_container& boundaries,
+                                   const Dimension_container& dimensions,
+                                   const Filtration_value_container& filtrationValues)
+      : boundaries_(boundaries), dimensions_(dimensions), filtrationValues_(filtrationValues), maxDimension_(0)
+  {
+    _initialize_dimension_utils();
+  }
 
-  bool filtration_is_one_critical() const;
+  // assumes boundary Idxs corresponds to container Idxs
+  Multi_parameter_filtered_complex(Boundary_container&& boundaries,
+                                   Dimension_container&& dimensions,
+                                   Filtration_value_container&& filtrationValues)
+      : boundaries_(std::move(boundaries)),
+        dimensions_(std::move(dimensions)),
+        filtrationValues_(std::move(filtrationValues)),
+        maxDimension_(0)
+  {
+    _initialize_dimension_utils();
+  }
 
-  const Filtration_value_container& get_filtration_values() const;
-  Filtration_value_container& get_filtration_values();
+  Index get_number_of_cycle_generators() const { return boundaries_.size(); }
 
-  const std::vector<Dimension>& get_dimensions() const;
-  // std::vector<Dimension>& get_dimensions();
+  Index get_number_of_parameters() const
+  {
+    if (filtrationValues_.empty()) return 0;
+    filtrationValues_[0].num_parameters();
+  }
 
-  const Boundary_container& get_boundaries() const;
-  // Boundary_container& get_boundaries();
+  bool is_ordered_by_dimension() const { return isOrderedByDimension_; }
 
-  Dimension get_dimension(Index i) const;
-  Dimension get_max_dimension() const;
+  const Filtration_value_container& get_filtration_values() const { return filtrationValues_; }
 
-  int prune_above_dimension(int maxDim);
+  Filtration_value_container& get_filtration_values() { return filtrationValues_; }
 
-  void coarsen_on_grid(const std::vector<std::vector<T>> &grid, bool coordinate = true);
+  const Dimension_container& get_dimensions() const { return dimensions_; }
 
-  // Warning: be carefull that the permutation preserves ordered by dim
+  Dimension_container& get_dimensions() { return dimensions_; }
+
+  const Boundary_container& get_boundaries() const { return boundaries_; }
+
+  Boundary_container& get_boundaries() { return boundaries_; }
+
+  Dimension get_dimension(Index i) const { return dimensions_[i]; }
+
+  Dimension get_max_dimension() const { return maxDimension_; }
+
+  void sort_by_dimension_co_lexicographically()
+  {
+    sort([&](Index i, Index j) -> bool {
+      if (dimensions_[i] == dimensions_[j]) {
+        return multi_filtration::is_strict_less_than_lexicographically<true>(filtrationValues_[i],
+                                                                             filtrationValues_[j]);
+      }
+      return dimensions_[i] < dimensions_[j];
+    });
+
+    isOrderedByDimension_ = true;
+  }
+
+  template <typename Comp>
+  void sort(Comp&& comparaison)
+  {
+    std::vector<Index> perm(boundaries_.size());
+    std::iota(perm.begin(), perm.end(), 0);
+    std::vector<Index> pos = perm;
+    std::sort(perm.begin(), perm.end(), comparaison);
+    std::vector<Index> inv(boundaries_.size());
+    for (Index i = 0; i < perm.size(); ++i) inv[perm[i]] = i;
+
+    for (Index curr = 0; curr < perm.size(); ++curr) {
+      Index p = perm[curr];
+      Index i = pos[p];
+      if (i != curr) {
+        GUDHI_CHECK(curr < i, "Something is wrong");
+        std::swap(boundaries_[curr], boundaries_[i]);
+        for (Index& b : boundaries_[curr]) b = inv[b];
+        std::swap(dimensions_[curr], dimensions_[i]);
+        swap(filtrationValues_[curr], filtrationValues_[i]);
+        std::swap(pos[curr], pos[i]);
+      }
+    }
+  }
+
+  // warning: shuffles order if not ordered by dimension
+  int prune_above_dimension(int maxDim)
+  {
+    if (!isOrderedByDimension_) sort_by_dimension_co_lexicographically();
+    Index i = 0;
+    while (i < dimensions_.size() && dimensions_[i] < maxDim + 1) ++i;
+    boundaries_.resize(i);
+    dimensions_.resize(i);
+    filtrationValues_.resize(i);
+    maxDimension_ = dimensions_.empty() ? -1 : dimensions_.back();
+  }
+
+  void coarsen_on_grid(const std::vector<std::vector<T> >& grid, bool coordinate = true)
+  {
+    for (auto gen = 0u; gen < filtrationValues_.size(); ++gen) {
+      filtrationValues_[gen].project_onto_grid(grid, coordinate);
+    }
+  }
+
   friend Multi_parameter_filtered_complex build_permuted_complex(const Multi_parameter_filtered_complex& complex,
-                                                                 const std::vector<Index>& permutation);
+                                                                 const std::vector<Index>& permutation)
+  {
+    if (permutation.size() != complex.get_number_of_cycle_generators())
+      throw std::invalid_argument("Invalid permutation size.");
+
+    std::vector<Index> inv(permutation.size());
+    for (Index i = 0; i < permutation.size(); ++i) inv[permutation[i]] = i;
+
+    Boundary_container newBoundaries;
+    newBoundaries.reserve(permutation.size());
+    Dimension_container newDimensions;
+    newDimensions.reserve(permutation.size());
+    Filtration_value_container newFiltrationValues;
+    newBoundaries.reserve(permutation.size());
+
+    for (Index i : permutation) {
+      Boundary boundary(complex.boundaries_[i]);
+      for (Index& b : boundary) b = inv[b];
+      newBoundaries.emplace_back(std::move(boundary));
+      newDimensions.push_back(complex.dimensions_[i]);
+      newFiltrationValues.emplace_back(complex.filtrationValues_[i]);
+    }
+
+    return Multi_parameter_filtered_complex(
+        std::move(newBoundaries), std::move(newDimensions), std::move(newFiltrationValues));
+  }
 
   friend std::pair<Multi_parameter_filtered_complex, std::vector<Index> > build_permuted_complex(
-      const Multi_parameter_filtered_complex& complex);
+      const Multi_parameter_filtered_complex& complex)
+  {
+    std::vector<Index> perm(complex.get_number_of_cycle_generators());
+    std::iota(perm.begin(), perm.end(), 0);
+    std::sort(perm.begin(), perm.end(), [&](Index i, Index j) -> bool {
+      if (complex.dimensions_[i] == complex.dimensions_[j]) {
+        return multi_filtration::is_strict_less_than_lexicographically<true>(complex.filtrationValues_[i],
+                                                                             complex.filtrationValues_[j]);
+      }
+      return complex.dimensions_[i] < complex.dimensions_[j];
+    });
+    return build_permuted_complex(complex, perm);
+  }
 
   friend auto build_complex_coarsen_on_grid(const Multi_parameter_filtered_complex& complex,
                                             const std::vector<std::vector<T> >& grid)
   {
-    // using return_filtration_value = decltype(std::declval<Filtration_value>().template as_type<std::int32_t>());
-    // using return_complex = Multi_parameter_filtered_complex<return_filtration_value>;
-    // typename return_complex::Filtration_value_container coords(slicer.get_number_of_cycle_generators());
-    // for (std::size_t gen = 0u; gen < coords.size(); ++gen) {
-    //   coords[gen] = compute_coordinates_in_grid<std::int32_t>(generator_filtration_values[gen], grid);
-    // }
-    // Multi_parameter_filtered_complex<return_filtration_value> outComplex();
+    using Return_filtration_value = decltype(std::declval<Filtration_value>().template as_type<std::int32_t>());
+    using Return_complex = Multi_parameter_filtered_complex<Return_filtration_value>;
+    typename Return_complex::Filtration_value_container coords(complex.get_number_of_cycle_generators());
+    for (std::size_t gen = 0u; gen < coords.size(); ++gen) {
+      coords[gen] = multi_filtration::compute_coordinates_in_grid<std::int32_t>(complex.filtrationValues_[gen], grid);
+    }
+    return Return_complex(complex.boundaries_, complex.dimensions_, coords);
   }
 
-  friend std::ostream& operator<<(std::ostream& stream, const Multi_parameter_filtered_complex& complex);
+  friend std::ostream& operator<<(std::ostream& stream, const Multi_parameter_filtered_complex& complex)
+  {
+    stream << "Boundary:\n";
+    stream << "{\n";
+    for (Index i = 0; i < complex.boundaries_.size(); ++i) {
+      const auto& boundary = complex.boundaries_[i];
+      stream << i << ": {";
+      for (auto b : boundary) stream << b << ", ";
+      if (!boundary.empty()) stream << "\b" << "\b ";
+      stream << "},\n";
+    }
+    stream << "}\n";
+
+    stream << "Dimensions: (max " << complex.get_max_dimension() << ")\n";
+    stream << "{";
+    for (auto d : complex.dimensions_) stream << d << ", ";
+    if (!complex.dimensions_.empty()) {
+      stream << "\b" << "\b";
+    }
+    stream << "}\n";
+
+    stream << "Filtration values:\n";
+    stream << "{";
+    for (auto f : complex.filtrationValues_) stream << f << "\n";
+    stream << "}\n";
+
+    return stream;
+  }
+
+ private:
+  Boundary_container boundaries_;
+  Dimension_container dimensions_;
+  Filtration_value_container filtrationValues_;
+  Dimension maxDimension_;
+  bool isOrderedByDimension_;
+
+  void _initialize_dimension_utils()
+  {
+    isOrderedByDimension_ = true;
+    for (Index i = 0; i < dimensions_.size() - 1; ++i) {
+      maxDimension_ = std::max(dimensions_[i], maxDimension_);
+      if (dimensions_[i] > dimensions_[i + 1]) isOrderedByDimension_ = false;
+    }
+    maxDimension_ = std::max(dimensions_.back(), maxDimension_);
+  }
 };
 
 }  // namespace multi_persistence
--- src/Multi_persistence/include/gudhi/Projective_cover_kernel.h
+++ src/Multi_persistence/include/gudhi/Projective_cover_kernel.h
@@ -19,19 +19,21 @@
 #define MP_PROJECTIVE_COVER_KERNEL_H_INCLUDED
 
 // #include <utility>  //std::move
-// #include <vector>
+#include <vector>
+#include <set>
+#include "gudhi/Multi_parameter_filtration.h"
 
 #include <gudhi/Debug_utils.h>
 #include <gudhi/Multi_parameter_filtered_complex.h>
 #include <gudhi/Matrix.h>
 
-#include <gudhi/Multi_parameter_filtration.h>
+// #include <gudhi/Multi_parameter_filtration.h>
 
 namespace Gudhi {
 namespace multi_persistence {
 
-// template <class Multi_filtration_value>
-template <Gudhi::persistence_matrix::Column_types columnType = Gudhi::persistence_matrix::Column_types::NAIVE_VECTOR>
+template <class Multi_filtration_value,
+          Gudhi::persistence_matrix::Column_types columnType = Gudhi::persistence_matrix::Column_types::NAIVE_VECTOR>
 class Projective_cover_kernel
 {
  private:
@@ -40,22 +42,36 @@ class Projective_cover_kernel
   };
 
  public:
-  // using Filtration_value = Multi_filtration_value;
-  using Filtration_value = Gudhi::multi_filtration::Multi_parameter_filtration<double>;
+  using Filtration_value = Multi_filtration_value;
   using Complex = Multi_parameter_filtered_complex<Filtration_value>;
   using Index = typename Complex::Index;
   using Dimension = typename Complex::Dimension;
+  using Filtration_value_container = typename Complex::Filtration_value_container;
+  using Boundary_container = typename Complex::Boundary_container;
+  using Dimension_container = typename Complex::Dimension_container;
   using Matrix = Gudhi::persistence_matrix::Matrix<Matrix_options>;
 
   // TODO : this only works for 2 parameter modules. Optimize w.r.t. this.
   // filtration values are assumed to be dim + co-lexicographically sorted
-  Projective_cover_kernel(const Complex& complex, Dimension dim) : complex_(&complex)
+  Projective_cover_kernel(const Complex &complex, Dimension dim)
   {
     if (complex.get_number_of_parameters() != 2) throw std::invalid_argument("Only available for 2-parameter modules.");
-    if (complex.filtration_is_one_critical()) throw std::invalid_argument("Only available for 1-critical modules.");
 
-    const auto& boundaries = complex.get_boundaries();
-    const auto& filtValues = complex.get_filtration_values();
+    const auto &boundaries = complex.get_boundaries();
+    const auto &filtValues = complex.get_filtration_values();
+    const auto &dimensions = complex.get_dimensions();
+
+    GUDHI_CHECK_code(
+      Index i = 0;
+      for (; i < complex.get_number_of_cycle_generators() - 1; ++i){
+        GUDHI_CHECK(filtValues[i].num_generators() == 1, "Only available for 1-critical modules.");
+        GUDHI_CHECK(dimensions[i] <= dimensions[i+1], "Cells have to be ordered by dimension.");
+        if (dimensions[i] == dimensions[i + 1])
+          GUDHI_CHECK(multi_filtration::is_less_or_equal_than_lexicographically<true>(filtValues[i], filtValues[i + 1]),
+                      "Cells with same dimension have to be ordered co-lexicographically.");
+      }
+      GUDHI_CHECK(filtValues[i].num_generators() == 1, "Only available for 1-critical modules.");
+    )
 
     Index startDim1, startDim2, end;
     _get_complex_indices(complex, dim, startDim1, startDim2, end);
@@ -63,24 +79,37 @@ class Projective_cover_kernel
     Index nberDim = startDim2 - startDim1;
     Index nberDim1 = end - startDim2;
     Index nberGen = nberDim + nberDim1;
+    Index shift = startDim1;
 
-    SmallQueue lexico_it;
     Matrix M(nberGen);
     Matrix N(nberGen);  // slave
-    for (int i = startDim1; i < end; i++) {
-      M.insert_boundary(boundaries[i]);
-      N.insert_boundary({i});
-    }
-
-    boundaries_.reserve(nberDim1);
-    filtrationValues_.reserve(nberDim1);
-    dimensions_.reserve(nberDim1);
-
-    auto get_pivot = [&M, startDim1](Index i) { return _get_pivot(M, i, startDim1); };
-
     std::vector<bool> isReduced(nberGen);
-    auto pivotCache = _initialize_pivot_cache(isReduced, nberDim, nberGen, get_pivot);
+    // TODO : pivot caches are small : maybe use a flat container instead ?
+    std::vector<std::set<int>> pivotCache(nberGen);  // this[pivot] = cols of given pivot
+
+    auto get_pivot = [&M, shift](Index i) { return _get_pivot(M, i, shift); };
+
+    _initialize_matrices(startDim1, end, boundaries, M, N);
+    _initialize_containers(nberDim, nberGen, shift, filtValues, dimensions, get_pivot, pivotCache, isReduced);
+    SmallQueue lexicoIt = _initialize_lex_queue(nberDim, nberGen, shift, filtValues, pivotCache, get_pivot);
+
+    while (!lexicoIt.empty()) {
+      Filtration_value gridValue = std::move(lexicoIt.pop());
+      for (int i : lexicoIt.get_current_cols()) {
+        while (_reduce_column(complex, i, isReduced, pivotCache, M, N, gridValue, get_pivot));
+        _update_after_new_pivot(i, lexicoIt, pivotCache, filtValues, gridValue, get_pivot);
+      }
+    }
   }
+
+  Boundary_container build_generators()
+  {
+    Index start = 0;
+    while (dimensions_[start] == dimensions_[0]) ++start;
+    return Boundary_container(boundaries_.begin() + start, boundaries_.end());
+  }
+
+  Complex create_complex() { return Complex(boundaries_, dimensions_, filtrationValues_); }
 
  private:
   struct SmallQueue {
@@ -92,7 +121,8 @@ class Projective_cover_kernel
       MFWrapper(const Filtration_value &g, int col) : g(g) { some_cols.insert(col); }
 
       MFWrapper(const Filtration_value &g, std::initializer_list<int> cols) : g(g), some_cols(cols.begin(), cols.end())
-      {}
+      {
+      }
 
       inline void insert(int col) const { some_cols.insert(col); }
 
@@ -142,16 +172,15 @@ class Projective_cover_kernel
     std::set<int> last_cols;
   };
 
-  const Complex* complex_;
-  std::vector<std::vector<Index> > boundaries_;
-  std::vector<Filtration_value> filtrationValues_;
-  std::vector<Dimension> dimensions_;
+  Boundary_container boundaries_;
+  Dimension_container dimensions_;
+  Filtration_value_container filtrationValues_;
 
-  static void _get_complex_indices(const Complex& complex,
+  static void _get_complex_indices(const Complex &complex,
                                    Dimension dim,
-                                   Index& startDim1,
-                                   Index& startDim2,
-                                   Index& end)
+                                   Index &startDim1,
+                                   Index &startDim2,
+                                   Index &end)
   {
     startDim1 = 0;
     startDim2 = 0;
@@ -176,23 +205,138 @@ class Projective_cover_kernel
     return col.size() > 0 ? (*col.rbegin()).get_row_index() - shift : -1;
   }
 
-  template <typename F>
-  static std::vector<std::set<int>> _initialize_pivot_cache(std::vector<bool> &isReduced,
-                                                            Index nberDim,
-                                                            Index nberGen,
-                                                            F &&get_pivot)
+  static void _initialize_matrices(Index start, Index end, const Boundary_container &boundaries, Matrix &M, Matrix &N)
   {
-    // TODO : pivot caches are small : maybe use a flat container instead ?
-    std::vector<std::set<int>> pivot_cache(nberGen);  // this[pivot] = cols of given pivot (<=nd)
-    for (Index j = nberDim; j < nberGen; ++j) {
-      int pivot = get_pivot(j);
-      if (pivot < 0) {
-        isReduced[j] = true;
-      } else {
-        auto &currentCache = pivot_cache[pivot];
-        currentCache.emplace_hint(currentCache.cend(), j);  // j is increasing
+    for (int i = start; i < end; i++) {
+      M.insert_boundary(boundaries[i]);
+      N.insert_boundary({i - start});
+    }
+  }
+
+  template <typename F>
+  static SmallQueue _initialize_lex_queue(Index nberDim,
+                                          Index nberGen,
+                                          Index shift,
+                                          const Filtration_value_container &filtValues,
+                                          const std::vector<std::set<int>> &pivotCache,
+                                          F &&get_pivot)
+  {
+    SmallQueue lexicoIt;
+    for (Index i = nberDim; i < nberGen; ++i) {
+      int pivot = get_pivot(i);
+      if (pivot >= 0) {
+        lexicoIt.insert(filtValues[i + shift], i);
+        auto it = pivotCache[pivot].find(i);
+        GUDHI_CHECK(it != pivotCache[pivot].end(), "Column not registered in pivot cache.");
+        it++;
+        for (; it != pivotCache[pivot].end(); ++it) {
+          int colIdx = *it;
+          GUDHI_CHECK(colIdx > i, "Column not registered in the right order.");
+          auto prev = filtValues[colIdx + shift];
+          prev.push_to_least_common_upper_bound(filtValues[i + shift]);
+          lexicoIt.insert(std::move(prev), colIdx);
+        }
       }
     }
+    return lexicoIt;
+  }
+
+  template <typename F>
+  static void _update_after_new_pivot(Index i,
+                                      SmallQueue &lexicoIt,
+                                      std::vector<std::set<int>> &pivotCache,
+                                      const Filtration_value_container &filtValues,
+                                      const Filtration_value &gridValue,
+                                      F &&get_pivot)
+  {
+    int pivot = get_pivot(i);
+
+    if (pivot < 0) return;
+
+    auto [it, wasThere] = pivotCache[pivot].insert(i);
+    it++;
+    for (; it != pivotCache[pivot].end(); ++it) {
+      int colIdx = *it;
+      GUDHI_CHECK(colIdx > i, "(update) Column not registered in the right order.");
+      auto prev = filtValues[colIdx];
+      if (prev < filtValues[i]) {
+        prev.push_to_least_common_upper_bound(filtValues[i]);
+        if (is_strict_less_than_lexicographically(gridValue, prev)) {
+          lexicoIt.insert(prev, colIdx);
+        }
+      }
+    }
+  }
+
+  template <typename F>
+  void _initialize_containers(Index nberDim,
+                              Index nberGen,
+                              Index shift,
+                              const Filtration_value_container &filtValues,
+                              const Dimension_container &dimensions,
+                              F &&get_pivot,
+                              std::vector<std::set<int>> &pivotCache,
+                              std::vector<bool> &isReduced)
+  {
+    Index size = (nberGen - nberDim) * 2;
+    boundaries_.reserve(size);
+    filtrationValues_.reserve(size);
+    dimensions_.reserve(size);
+
+    for (Index i = nberDim; i < nberGen; ++i) {
+      boundaries_.emplace_back();
+      filtrationValues_.push_back(filtValues[i + shift]);
+      dimensions_.push_back(dimensions[i + shift]);
+      int pivot = get_pivot(i);
+      if (pivot < 0) {
+        isReduced[i] = true;
+      } else {
+        auto &currentCache = pivotCache[pivot];
+        currentCache.emplace_hint(currentCache.cend(), i);  // j is increasing
+      }
+    }
+  }
+
+  template <typename F>
+  bool _reduce_column(const Complex &complex,
+                      Index i,
+                      std::vector<bool> &isReduced,
+                      std::vector<std::set<int>> &pivotCache,
+                      Matrix &M,
+                      Matrix &N,
+                      const Filtration_value &gridValue,
+                      F &&get_pivot)
+  {
+    int pivot = get_pivot(i);
+    if (pivot < 0) {
+      if (!isReduced[i]) {
+        const auto &col = N.get_column(i);
+        boundaries_.emplace_back(col.begin(), col.end());
+        filtrationValues_.emplace_back(gridValue);
+        dimensions_.emplace_back(complex.get_dimension(i) + 1);
+        isReduced[i] = true;
+      }
+      return false;
+    }
+    // WARN : we lazy update variables linked with col i...
+    if (pivotCache[pivot].size() == 0) {
+      return false;
+    }
+    const auto &filtValues = complex.get_filtration_values();
+    for (int k : pivotCache[pivot]) {
+      if (k >= i) {  // cannot reduce more here. this is a (local) pivot.
+        return false;
+      }
+      if (filtValues[k] <= gridValue) {
+        M.add_to(k, i);
+        N.add_to(k, i);
+        pivotCache[pivot].erase(i);
+        // WARN : we update the pivot cache after the update loop
+        GUDHI_CHECK(get_pivot(i) < pivot, "Column addition failed.");
+        return true;  // pivot has changed
+      }
+    }
+    return false;  // for loop exhausted (i may not be there because of lazy)
   }
 };
 
