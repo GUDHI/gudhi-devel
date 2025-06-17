6fd1c24514d5754e4943b856859f48e5ce08a45e
hschreiber <hannah.schreiber.k@googlemail.com>
2025-06-17 21:51:35 +0200

    inline + GUDHI_DEBUG + other cleanup common_persistence_representation.h

--- src/Persistence_representations/include/gudhi/common_persistence_representations.h
+++ src/Persistence_representations/include/gudhi/common_persistence_representations.h
@@ -17,16 +17,16 @@
 #ifndef COMMON_PERSISTENCE_REPRESENTATIONS_H_
 #define COMMON_PERSISTENCE_REPRESENTATIONS_H_
 
-#include <utility>
-#include <string>
-#include <cmath>
+#include <stdexcept>  // std::invalid_argument
+#include <utility>    // std::pair, std::make_pair
+#include <sstream>    // std::ostringstream
+#include <cmath>      // std::fabs
+#include <vector>
 
 #include <boost/math/constants/constants.hpp>
 
 namespace Gudhi {
 namespace Persistence_representations {
-
-static constexpr double pi = boost::math::constants::pi<double>();
 
 /**
  * In this module, we use the name Persistence_diagram for the representation of a diagram in a vector of pairs of two
@@ -34,13 +34,24 @@ static constexpr double pi = boost::math::constants::pi<double>();
  */
 using Persistence_diagram = std::vector<std::pair<double, double> >;
 
-// double epsi = std::numeric_limits<double>::epsilon();
-double epsi = 0.000005;
+/**
+ * Pi value.
+ */
+inline constexpr double pi = boost::math::constants::pi<double>();
+
+// TODO: I don't think this is a good idea? Having modifiable global variables like that.
+// But the description of `almost_equal` shows that it is the intended use.
+// At the same time, it seems to be the only method using it. Can we not add it as an argument instead?
+/**
+ * Epsilon value for rounding tolerances. Initialized to 0.000005.
+ * The value can be modified if another tolerance threshold is needed.
+ */
+inline double epsi = 0.000005;
 
 /**
- * A procedure used to compare doubles. Typically given two doubles A and B, comparing A == B is not good idea.
- * In this case, we use the procedure almostEqual with the epsi defined at the top of the file.
- * Setting up the epsi gives the user a tolerance on what should be consider equal.
+ * A procedure used to compare doubles. Typically given two doubles \f$ A \f$ and \f$ B \f$, comparing both with
+ * \f$ A == B \f$ is not good idea. In this case, we use the procedure @ref almost_equal with the global variable
+ * @ref epsi "". Setting up @ref epsi gives the user a tolerance on what should be consider equal.
  **/
 inline bool almost_equal(double a, double b)
 {
@@ -50,54 +61,22 @@ inline bool almost_equal(double a, double b)
 
 // landscapes
 /**
- * Extra functions needed in construction of barcodes.
+ * @private Extra functions needed in construction of barcodes.
  **/
-double minus_length(std::pair<double, double> a) { return a.first - a.second; }
+inline constexpr double minus_length(const std::pair<double, double>& a) { return a.first - a.second; }
 
-double birth_plus_deaths(std::pair<double, double> a) { return a.first + a.second; }
+/**
+ * @private Extra functions needed in construction of barcodes.
+ */
+inline constexpr double birth_plus_deaths(const std::pair<double, double>& a) { return a.first + a.second; }
 
 // landscapes
 /**
- * Given two points in R^2, the procedure compute the parameters A and B of the line y = Ax + B that crosses those two
- * points.
- **/
-std::pair<double, double> compute_parameters_of_a_line(std::pair<double, double> p1, std::pair<double, double> p2)
-{
-  double a = (p2.second - p1.second) / (p2.first - p1.first);
-  double b = p1.second - a * p1.first;
-  return std::make_pair(a, b);
-}
-
-// landscapes
-/**
- * This procedure given two points which lies on the opposite sides of x axis, compute x for which the line connecting
- * those two points crosses x axis.
- **/
-double find_zero_of_a_line_segment_between_those_two_points(std::pair<double, double> p1, std::pair<double, double> p2)
-{
-  if (p1.first == p2.first) return p1.first;
-  if (p1.second * p2.second > 0) {
-    std::ostringstream errMessage;
-    errMessage << "In function find_zero_of_a_line_segment_between_those_two_points the arguments are: (" << p1.first
-               << "," << p1.second << ") and (" << p2.first << "," << p2.second
-               << "). There is no zero in line between those two points. Program terminated.";
-    std::string errMessageStr = errMessage.str();
-    const char* err = errMessageStr.c_str();
-    throw(err);
-  }
-  // we assume here, that x \in [ p1.first, p2.first ] and p1 and p2 are points between which we will put the line
-  // segment
-  double a = (p2.second - p1.second) / (p2.first - p1.first);
-  double b = p1.second - a * p1.first;
-  return -b / a;
-}
-
-// landscapes
-/**
+ * @private
  * This method provides a comparison of points that is used in construction of persistence landscapes. The ordering is
  * lexicographical for the first coordinate, and reverse-lexicographical for the second coordinate.
  **/
-bool compare_points_sorting(std::pair<double, double> f, std::pair<double, double> s)
+inline constexpr bool compare_points_sorting(const std::pair<double, double>& f, const std::pair<double, double>& s)
 {
   if (f.first < s.first) {
     return true;
@@ -116,15 +95,49 @@ bool compare_points_sorting(std::pair<double, double> f, std::pair<double, doubl
 
 // landscapes
 /**
- * This procedure takes two points in R^2 and a double value x. It computes the line parsing through those two points
- * and return the value of that linear function at x.
+ * Given two points in \f$ R^2 \f$, the procedure compute the parameters \f$ A \f$ and \f$ B \f$ of the line
+ * \f$ y = Ax + B \f$ that goes through those two points.
  **/
-double function_value(std::pair<double, double> p1, std::pair<double, double> p2, double x)
+inline std::pair<double, double> compute_parameters_of_a_line(const std::pair<double, double>& p1,
+                                                              const std::pair<double, double>& p2)
+{
+  const double a = (p2.second - p1.second) / (p2.first - p1.first);
+  const double b = p1.second - a * p1.first;
+  return std::make_pair(a, b);
+}
+
+// landscapes
+/**
+ * This procedure, given two points which lies on the opposite sides of the \f$ x \f$-axis, computes the value of
+ * \f$ x \f$ for which the line connecting those two points crosses the \f$ x \f$-axis.
+ **/
+inline double find_zero_of_a_line_segment_between_those_two_points(const std::pair<double, double>& p1,
+                                                                   const std::pair<double, double>& p2)
+{
+  if (p1.first == p2.first) return p1.first;
+  if (p1.second * p2.second > 0) {
+    std::ostringstream errMessage;
+    errMessage << "In function find_zero_of_a_line_segment_between_those_two_points the arguments are: (" << p1.first
+               << "," << p1.second << ") and (" << p2.first << "," << p2.second
+               << "). There is no zero in line between those two points. Program terminated.";
+    throw std::invalid_argument(errMessage.str());
+  }
+  // we assume here, that x \in [ p1.first, p2.first ] and p1 and p2 are points between which we will put the line
+  // segment
+  const auto [a, b] = compute_parameters_of_a_line(p1, p2);
+  return -b / a;
+}
+
+// landscapes
+/**
+ * This procedure takes two points in \f$ R^2 \f$ and a double value \f$ x \f$. It computes the line parsing through
+ * those two points and return the value of that linear function at \f$ x \f$.
+ **/
+inline double function_value(const std::pair<double, double>& p1, const std::pair<double, double>& p2, double x)
 {
   // we assume here, that x \in [ p1.first, p2.first ] and p1 and p2 are points between which we will put the line
   // segment
-  double a = (p2.second - p1.second) / (p2.first - p1.first);
-  double b = p1.second - a * p1.first;
+  const auto [a, b] = compute_parameters_of_a_line(p1, p2);
   return (a * x + b);
 }
 
