8e71fc8b4c4906f232bdbe5084a340a0760aa43e
Vincent Rouvreau <vincent.rouvreau@inria.fr>
2025-10-14 12:04:41 +0200

    fix some __all__

--- src/python/gudhi/sklearn/__init__.py
+++ src/python/gudhi/sklearn/__init__.py
@@ -1,8 +1,18 @@
 from .cubical_persistence import CubicalPersistence
 from .rips_persistence import RipsPersistence
 
+__all__ = [
+    'CubicalPersistence',
+    'RipsPersistence',
+]
+
 try:
     # if no CGAL
     from .cech_persistence import CechPersistence, WeightedCechPersistence
+    
+    __all__ += [
+        'CechPersistence',
+        'WeightedCechPersistence',
+    ]
 except ImportError:
     pass
