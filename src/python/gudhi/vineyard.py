d756463f9e6da3e6143b5639b831a74b16a4738b
hschreiber <hannah.schreiber.k@googlemail.com>
2025-12-15 19:40:28 +0100

    revisit color in vineyard plotting + thresholds for getters

--- src/python/gudhi/vineyard.py
+++ src/python/gudhi/vineyard.py
@@ -14,6 +14,7 @@ import os
 import glob
 import numpy as np
 from numpy.typing import ArrayLike
+from typing import Literal
 import warnings
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -167,18 +168,57 @@ class Vineyard(t.Vineyard_interface):
 
         return self._denoise_vineyard(vineyard, min_bar_length)
 
-    def _gray_on_diagonal(self, ax, x, y, z):
+    def _gray_on_band(self, ax, x, y, z, band):
         gray = (0.75, 0.75, 0.75)
-        mask = np.concatenate(([0], np.asarray(x == y), [0]))
+        mask = np.concatenate(([0], np.asarray(y - x <= band), [0]))
         diff = np.diff(mask)
         starts = np.where(diff == 1)[0]
         ends = np.where(diff == -1)[0]
         for s, e in zip(starts, ends):
             ax.plot3D(x[s:e], y[s:e], z[s:e], c=gray)
 
+    def _erase_on_band(self, ax, x, y, z, band, c):
+        mask = np.concatenate(([0], np.asarray(y - x > band), [0]))
+        diff = np.diff(mask)
+        starts = np.where(diff == 1)[0]
+        ends = np.where(diff == -1)[0]
+        for s, e in zip(starts, ends):
+            ax.plot3D(x[s:e], y[s:e], z[s:e], c=c)
+
+    def _get_colors_for_all_dim(self, dim: int):
+        plot_colors = [
+            "Oranges",
+            "Purples",
+            "Greens",
+            "Blues",
+            "Reds",
+            "YlOrBr",
+            "PuRd",
+            "YlOrRd",
+            "OrRd",
+            "RdPu",
+            "BuPu",
+            "GnBu",
+            "PuBu",
+            "YlGnBu",
+            "PuBuGn",
+            "BuGn",
+            "YlGn",
+        ]
+        return mpl.colormaps[plot_colors[dim % len(plot_colors)]]
+
     def _plot_vines(
-        self, ax, vines, inf_v, min_bar_length, gray_diagonal, c=None, c_step=None
+        self,
+        ax,
+        vines,
+        inf_v,
+        min_bar_length,
+        noise_option: Literal[
+            "none", "gray_diagonal", "gray_band", "erase_diagonal", "erase_band"
+        ],
+        cmap,
     ):
+        real_vines = []
         for i in range(vines.shape[0]):
             x = vines[i, :, 0]
             y = vines[i, :, 1]
@@ -188,18 +228,40 @@ class Vineyard(t.Vineyard_interface):
             diff = y - x
             diff[diff < min_bar_length] = 0
             if np.any(diff):
-                ax.plot3D(x, y, z, c=c)
-                if gray_diagonal:
-                    self._gray_on_diagonal(ax, x, y, z)
-                if c_step is not None:
-                    c = c - c_step
+                real_vines.append((x, y, z))
+
+        cmap = list(reversed(cmap(np.linspace(0, 1, len(real_vines) + 2))))
+
+        for i, (x, y, z) in enumerate(real_vines):
+            match noise_option:
+                case "none":
+                    ax.plot3D(x, y, z, c=cmap[i])
+                case "gray_diagonal":
+                    ax.plot3D(x, y, z, c=cmap[i])
+                    self._gray_on_band(ax, x, y, z, 0)
+                case "gray_band":
+                    ax.plot3D(x, y, z, c=cmap[i])
+                    self._gray_on_band(ax, x, y, z, min_bar_length)
+                case "erase_diagonal":
+                    self._erase_on_band(ax, x, y, z, 0, cmap[i])
+                case "erase_band":
+                    self._erase_on_band(ax, x, y, z, min_bar_length, cmap[i])
+                case _:
+                    raise ValueError(
+                        "argument `noise_option` does not contain a valid literal: "
+                        + noise_option
+                        + ". Possibilities are: `none`, `gray_diagonal`, `gray_band`, `erase_diagonal` and `erase_band`"
+                    )
 
     def plot_vineyards(
         self,
         dim: int = None,
         max_dim: int = None,
         min_bar_length: np.number = -1,
-        gray_diagonal: bool = True,
+        noise_option: Literal[
+            "none", "gray_diagonal", "gray_band", "erase_diagonal", "erase_band"
+        ] = "gray_diagonal",
+        square_scaling: bool = True,
     ):
         fig = plt.figure()
         ax = fig.add_subplot(projection="3d")
@@ -218,17 +280,24 @@ class Vineyard(t.Vineyard_interface):
             inf_v = max_death + 10
 
             for d, vines in enumerate(vineyard):
-                # very naive and non robust way to manage colors...
-                c = np.random.rand(3)
-                step = np.min(c / (vines.shape[1] + 1))
-                self._plot_vines(ax, vines, inf_v, min_bar_length, gray_diagonal, c, step)
+                cmap = self._get_colors_for_all_dim(d)
+                self._plot_vines(ax, vines, inf_v, min_bar_length, noise_option, cmap)
         else:
             vines = self.get_current_vineyard_view(dim=dim)
             max_death = np.max(vines[:, :, 1], where=~np.isinf(vines[:, :, 1]), initial=-1)
             # arbitrary, just to distance them a bit from the finite bars
             inf_v = max_death + 10
+            self._plot_vines(
+                ax, vines, inf_v, min_bar_length, noise_option, mpl.colormaps["viridis"]
+            )
 
-            self._plot_vines(ax, vines, inf_v, min_bar_length, gray_diagonal)
+        if square_scaling:
+            scaling = np.array([getattr(ax, "get_{}lim".format(dim))() for dim in "xy"])
+            ax.auto_scale_xyz(
+                *[[np.min(scaling), np.max(scaling)]] * 2,
+                [getattr(ax, "get_{}lim".format("z"))()],
+            )
+        ax.set_aspect("equalxy", "box")
 
         plt.show()
         return
@@ -411,8 +480,22 @@ class PointCloudRipsVineyard:
     ) -> list[np.ndarray] | np.ndarray:
         return self._vineyard.get_current_vineyard(dim, min_bar_length)
 
-    def get_complex(self) -> tuple[list[np.ndarray], np.ndarray]:
-        return t._build_boundary_matrix_from_complex(self._complex)
+    def _get_vertices(self, cpx: list[np.ndarray], i: int) -> set[int]:
+        if cpx[i].shape[0] == 0:
+            return {i}
+        # not optimal for high dimensions, but enables no restrictions on cpx order
+        return {v for idx in cpx[i] for v in self._get_vertices(cpx, idx)}
+
+    def get_complex(self, as_vertices: bool = False) -> tuple[list[np.ndarray], np.ndarray]:
+        cpx, dims = t._build_boundary_matrix_from_complex(self._complex)
+
+        if not as_vertices:
+            return cpx, dims
+        cpx = [
+            np.asarray(sorted([v for v in self._get_vertices(cpx, i)]))
+            for i, _ in enumerate(cpx)
+        ]
+        return cpx, dims
 
     def get_points(self, step: int = None) -> np.ndarray:
         if not self._store_points:
@@ -425,13 +508,13 @@ class PointCloudRipsVineyard:
         return self._points[step]
 
     def _denoise_cycle(
-        self, cycle: tuple[list[np.ndarray], np.number], min_bar_length: np.number
-    ):
+        self, cycle: list[tuple[np.ndarray, np.number]], min_bar_length: np.number
+    ) -> list[tuple[np.ndarray, np.number]]:
         return [c for c in cycle if c[1] >= min_bar_length]
 
     def get_1D_representative_cycles(
         self, step: int = None, min_bar_length: np.number = 0
-    ) -> list[tuple[list[np.ndarray], np.number]] | tuple[list[np.ndarray], np.number]:
+    ) -> list[list[tuple[np.ndarray, np.number]]] | list[tuple[np.ndarray, np.number]]:
         if not self._store_cycles:
             raise NotImplementedError(
                 "Cycles cannot be retrieved if the store options is at False."
@@ -447,12 +530,18 @@ class PointCloudRipsVineyard:
         return self._denoise_cycle(self._cycles[step], min_bar_length)
 
     def plot_vineyards(
-        self, dim: int = None, max_dim: int = None, min_bar_length: np.number = -1
+        self,
+        dim: int = None,
+        max_dim: int = None,
+        min_bar_length: np.number = -1,
+        noise_option: Literal[
+            "none", "gray_diagonal", "gray_band", "erase_diagonal", "erase_band"
+        ] = "gray_diagonal",
+        square_scaling: bool = True,
     ):
-        self._vineyard.plot_vineyards(dim, max_dim, min_bar_length)
+        self._vineyard.plot_vineyards(dim, max_dim, min_bar_length, noise_option, square_scaling)
 
-    def _plot_cycle(self, axes, cycle, points, cpx):
-        c = np.random.rand(3)
+    def _plot_cycle(self, axes, cycle, points, cpx, c):
         for u, v in [(points[cpx[idx][0]], points[cpx[idx][1]]) for idx in cycle]:
             if points.shape[1] == 2:
                 axes.plot([u[0], v[0]], [u[1], v[1]], color=c)
@@ -474,24 +563,35 @@ class PointCloudRipsVineyard:
         else:
             raise ValueError("Plotting only possible in 2D and 3D.")
 
+        cmap_f = mpl.colormaps['tab10']
+        cmap = cmap_f(np.arange(10))
+        cmap_f = mpl.colormaps['Set2']
+        cmap = np.concatenate((cmap, cmap_f(np.arange(8))))
+        cmap_f = mpl.colormaps['Dark2']
+        cmap = np.concatenate((cmap, cmap_f(np.arange(8))))
+        csize = 10 + 8 + 8
+        i = 0
+
         if index is None:
             for cycle in cycles:
                 if cycle[1] >= min_bar_length:
-                    self._plot_cycle(axes, cycle[0], points, cpx)
+                    self._plot_cycle(axes, cycle[0], points, cpx, cmap[i])
+                    i = (i + 1) % csize
         else:
             if min_bar_length != -1:
                 warnings.warn(
                     "Specified argument `min_length` is ignored.",
                     UserWarning,
                 )
-            self._plot_cycle(axes, cycles[index][0], points, cpx)
+            self._plot_cycle(axes, cycles[index][0], points, cpx, cmap[i])
 
+        gray = (0.25, 0.25, 0.25)
         x = points[:, 0]
         y = points[:, 1]
         if points.shape[1] == 2:
-            axes.plot(x, y, linestyle="none", markersize=3, marker="o")
+            axes.plot(x, y, linestyle="none", markersize=3, marker="o", c=gray)
         else:
-            axes.plot(x, y, points[:, 2], linestyle="none", markersize=3, marker="o")
+            axes.plot(x, y, points[:, 2], linestyle="none", markersize=3, marker="o", c=gray)
 
         plt.show()
         return
