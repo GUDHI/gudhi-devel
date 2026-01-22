24482fac614788dfce8e461ffa6ff0a70eaddb7a
jlazovskis <jlazovskis@gmail.com>
2026-01-22 11:36:14 +0200

    skip template in print function

--- src/Persistence_matrix/example/example_remove_insert_swap_with_cycles.cpp
+++ src/Persistence_matrix/example/example_remove_insert_swap_with_cycles.cpp
@@ -24,8 +24,7 @@ struct RU_rep_cycles_options : Default_options<Column_types::INTRUSIVE_LIST, tru
 
 using RU_matrix = Gudhi::persistence_matrix::Matrix<RU_rep_cycles_options>;
 
-template <class Matrix>
-void print_current_representative_cycles(const Matrix& M) {
+void print_current_representative_cycles(const RU_matrix& M) {
   const auto& rc = M.get_representative_cycles();
   for (const auto& cycle : rc) {
     std::cout << M.get_column_dimension(cycle[0]);
@@ -42,27 +41,27 @@ void remove_insert_swap_with_cycles() {
   Matrix mp({{}, {}, {}, {}, {0, 1}, {0, 3}, {0, 2}, {1, 2}, {2, 3}});
 
   std::cout << "Representative cycles at input:\n";
-  print_current_representative_cycles<RU_matrix>(mp);
+  print_current_representative_cycles(mp);
 
   std::cout << "Representative cycles after swapping 6 and 7:\n";
   mp.vine_swap(6);
   mp.update_representative_cycles();
-  print_current_representative_cycles<RU_matrix>(mp);
+  print_current_representative_cycles(mp);
 
   std::cout << "Representative cycles after inserting 0-cell at position 4:\n";
   mp.insert_maximal_cell(4, {});
   mp.update_representative_cycles();
-  print_current_representative_cycles<RU_matrix>(mp);
+  print_current_representative_cycles(mp);
 
   std::cout << "Representative cycles after swapping 8 and 9:\n";
   mp.vine_swap(8);
   mp.update_representative_cycles();
-  print_current_representative_cycles<RU_matrix>(mp);
+  print_current_representative_cycles(mp);
 
   std::cout << "Representative cycles after removing 5:\n";
   mp.remove_maximal_cell(5);
   mp.update_representative_cycles();
-  print_current_representative_cycles<RU_matrix>(mp);
+  print_current_representative_cycles(mp);
 }
 
 int main() { remove_insert_swap_with_cycles<RU_matrix>(); }
