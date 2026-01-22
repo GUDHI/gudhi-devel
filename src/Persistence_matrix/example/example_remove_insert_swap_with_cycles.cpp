c96ce737bf16119440d79d1182ff809ce56fcd25
Vincent Rouvreau <vincent.rouvreau@inria.fr>
2026-01-22 16:20:07 +0100

    Fix conflict between the 2 merges

--- src/Persistence_matrix/example/example_remove_insert_swap_with_cycles.cpp
+++ src/Persistence_matrix/example/example_remove_insert_swap_with_cycles.cpp
@@ -25,7 +25,7 @@ struct RU_rep_cycles_options : Default_options<Column_types::INTRUSIVE_LIST, tru
 using RU_matrix = Gudhi::persistence_matrix::Matrix<RU_rep_cycles_options>;
 
 void print_current_representative_cycles(const RU_matrix& M) {
-  const auto& rc = M.get_representative_cycles();
+  const auto& rc = M.get_all_representative_cycles();
   for (const auto& cycle : rc) {
     std::cout << M.get_column_dimension(cycle[0]);
     std::cout << "-cycle: ";
@@ -45,22 +45,22 @@ void remove_insert_swap_with_cycles() {
 
   std::cout << "Representative cycles after swapping 6 and 7:\n";
   mp.vine_swap(6);
-  mp.update_representative_cycles();
+  mp.update_all_representative_cycles();
   print_current_representative_cycles(mp);
 
   std::cout << "Representative cycles after inserting 0-cell at position 4:\n";
   mp.insert_maximal_cell(4, {});
-  mp.update_representative_cycles();
+  mp.update_all_representative_cycles();
   print_current_representative_cycles(mp);
 
   std::cout << "Representative cycles after swapping 8 and 9:\n";
   mp.vine_swap(8);
-  mp.update_representative_cycles();
+  mp.update_all_representative_cycles();
   print_current_representative_cycles(mp);
 
   std::cout << "Representative cycles after removing 5:\n";
   mp.remove_maximal_cell(5);
-  mp.update_representative_cycles();
+  mp.update_all_representative_cycles();
   print_current_representative_cycles(mp);
 }
 
