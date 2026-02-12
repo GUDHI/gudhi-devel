729d4a78dd985e29cc2b3c34560776bba7741d3c
hschreiber <hannah.schreiber.k@googlemail.com>
2026-02-12 19:07:39 +0100

    vineyard unit test part1

--- src/Vineyard/test/test_vineyard_helper.cpp
+++ src/Vineyard/test/test_vineyard_helper.cpp
@@ -17,6 +17,10 @@
 
 #include <gudhi/vineyard_helper.h>
 
-// BOOST_AUTO_TEST_CASE_TEMPLATE(vy_initialization, Option, option_list) {
+#include "vy_test_utilities.h"
+
+using option_list = boost::mpl::list<Chain_vineyard_options, RU_vineyard_options>;
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(vy_initialization, Option, option_list) {
   
-// }
+}
