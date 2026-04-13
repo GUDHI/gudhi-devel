60bfaadb693db5ff8d6587dcde9b28de3314048d
Vincent Rouvreau <vincent.rouvreau@inria.fr>
2026-04-13 14:55:22 +0200

    Use Gudhi::random for benchmark_tm

--- src/Toplex_map/benchmark/benchmark_tm.cpp
+++ src/Toplex_map/benchmark/benchmark_tm.cpp
@@ -5,6 +5,7 @@
  *    Copyright (C) 2018  INRIA
  *
  *    Modification(s):
+ *      - 2026/04 Vincent Rouvreau: Replace std::random_device with Gudhi::random
  *      - YYYY/MM Author: Description of the modification
  */
 
@@ -15,6 +16,7 @@
 #include <gudhi/Simplex_tree.h>
 #include <gudhi/Toplex_map.h>
 #include <gudhi/Lazy_toplex_map.h>
+#include <gudhi/Random.h>
 
 using namespace Gudhi;
 
@@ -58,20 +60,19 @@ int nb_insert_simplex2 = 3000;
 int nb_membership2 = 400000;
 
 Simplex random_simplex(int n, std::size_t d) {
-  std::random_device rd;
-  std::mt19937 gen(rd());
-  std::uniform_int_distribution<std::size_t> dis(1, n);
+  // Do not use Gudhi::random::get_default_random() here. Reset seeds randomly on each call - like it was previously
+  // Otherwise num_maximal_simplices is quite small
+  Gudhi::random::Random<> rng;
   Simplex s;
-  while (s.size() < d) s.insert(dis(gen));
+  while (s.size() < d) s.insert(rng.get<std::size_t>(1, n));
   return s;
 }
 
 std::vector<Simplex> r_vector_simplices(int n, int max_d, int m) {
-  std::random_device rd;
-  std::mt19937 gen(rd());
-  std::uniform_int_distribution<std::size_t> dis(1, max_d);
+  // Do not use Gudhi::random::get_default_random() here. Reset seeds randomly on each call - like it was previously
+  Gudhi::random::Random<> rng;
   std::vector<Simplex> v;
-  for (int i = 0; i < m; i++) v.push_back(random_simplex(n, dis(gen)));
+  for (int i = 0; i < m; i++) v.push_back(random_simplex(n, rng.get<std::size_t>(1, max_d)));
   return v;
 }
 
@@ -104,13 +105,13 @@ void chrono(int n, int d) {
   auto c2 = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
 
   if (c3 > 0)
-    std::clog << c1 << "\t \t" << c2 << "\t \t" << c3 << "\t \t" << K.num_maximal_simplices() << std::endl;
+    std::clog << c1 << "\t \t" << c2 << "\t \t" << c3 << "\t \t" << K.num_maximal_simplices() << " - " << simplices_insert_simplex2.size() << " - " << simplices_membership2.size() << std::endl;
   else
-    std::clog << c1 << "\t \t" << c2 << "\t \tN/A\t \t" << K.num_maximal_simplices() << std::endl;
+    std::clog << c1 << "\t \t" << c2 << "\t \tN/A\t \t" << K.num_maximal_simplices() << " - " << simplices_insert_simplex2.size() << " - " << simplices_membership2.size() << std::endl;
 }
 
 int main() {
-  for (int d = 5; d <= 40; d += 5) {
+  for (int d = 5; d <= 10; d += 5) {
     std::clog << "d=" << d << " \t  Insertions \t   Membership \t Contractions \t        Size" << std::endl;
     std::clog << "T Map \t \t";
     chrono<Toplex_map>(n, d);
