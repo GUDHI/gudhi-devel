1fb868661c8cd8375a1347eee946e4cc8ec06cc2
Vincent Rouvreau <vincent.rouvreau@inria.fr>
2026-04-13 17:42:50 +0200

    Document Random

--- src/common/include/gudhi/Random.h
+++ src/common/include/gudhi/Random.h
@@ -2,7 +2,7 @@
  *    See file LICENSE or go to https://gudhi.inria.fr/licensing/ for full license details.
  *    Author(s):       Vincent Rouvreau
  *
- *    Copyright (C) 2024 Inria
+ *    Copyright (C) 2026 Inria
  *
  *    Modification(s):
  *      - YYYY/MM Author: Description of the modification
@@ -11,12 +11,10 @@
 #ifndef GUDHI_RANDOM_H_
 #define GUDHI_RANDOM_H_
 
-#include <random>  // for std::random_device, std::mt19937, std::uniform_real_distribution
+#include <random>  // for std::random_device, std::mt19937_64, std::uniform_real_distribution
 #include <vector>
 #include <algorithm>  // for std::generate
 #include <cstddef>  // for std::size_t
-#include <vector>
-#include <algorithm>  // for std::generate
 #ifdef DEBUG_TRACES
 #include <iostream>
 #endif  // DEBUG_TRACES
@@ -25,9 +23,24 @@ namespace Gudhi {
 
 namespace random {
   
+  /**
+   * @class Random
+   * @brief A generic random number generator class using C++11 random facilities.
+   *
+   * This class provides a flexible interface for generating random numbers of various types
+   * (integral and floating-point) and ranges, using a customizable random engine.
+   * If you do not need a specific one, consider using the one provided by `Gudhi::random::get_default_random()`.
+   *
+   * @tparam Engine The random engine type to use (default: `std::mt19937_64`).
+   */
   template<typename Engine = std::mt19937_64>
   class Random {
    public:
+    /**
+     * @brief Constructs a `Random` object with an optional seed. If no seed is provided, a random seed is generated.
+     *
+     * @param seed (Optional) The seed value for the random engine.
+     */
     explicit Random(typename Engine::result_type seed = std::random_device{}())
       : gen_(seed) {
 #ifdef DEBUG_TRACES
@@ -35,10 +48,23 @@ namespace random {
 #endif  // DEBUG_TRACES
     }
 
+    /**
+     * @brief Sets the seed for the random engine.
+     *
+     * @param seed The new seed value.
+     */
     void set_seed(typename Engine::result_type seed) {
       gen_.seed(seed);
     }
     
+    /**
+     * @brief Generates a random number in the range [min, max].
+     *
+     * @tparam Type The type of the random number (must be integral or floating-point).
+     * @param min The minimum value (inclusive).
+     * @param max The maximum value (inclusive).
+     * @return A random number of type `Type` in the range [min, max].
+     */
     template <typename Type>
     Type get(const Type& min, const Type& max) {
       if constexpr (std::is_floating_point_v<Type>) {
@@ -50,16 +76,38 @@ namespace random {
       }
     }
 
+    /**
+     * @brief Generates a random number in the range [0, max].
+     *
+     * @tparam Type The type of the random number (must be integral or floating-point).
+     * @param max The maximum value (inclusive).
+     * @return A random number of type `Type` in the range [0, max].
+     */
     template <typename Type>
     Type get(const Type& max) {
       return get<Type>(static_cast<Type>(0), max);
     }
 
+    /**
+     * @brief Generates a random number in the range [0, 1].
+     *
+     * @tparam Type The type of the random number (must be integral or floating-point).
+     * @return A random number of type `Type` in the range [0, 1].
+     */
     template <typename Type>
     Type get() {
       return get<Type>(static_cast<Type>(0), static_cast<Type>(1));
     }
 
+    /**
+     * @brief Generates a vector of N random numbers in the range [min, max].
+     *
+     * @tparam Type The type of the random numbers (must be integral or floating-point).
+     * @param nbr The number of random numbers to generate.
+     * @param min The minimum value (inclusive).
+     * @param max The maximum value (inclusive).
+     * @return A vector of random numbers of type `Type` in the range [min, max].
+     */
     template <typename Type>
     std::vector<Type> get_range(std::size_t nbr, const Type& min, const Type& max) {
       std::vector<Type> result(nbr);
@@ -73,17 +121,35 @@ namespace random {
       return result;
     }
     
+    /**
+     * @brief Generates a vector of N random numbers in the range [0, max].
+     *
+     * @tparam Type The type of the random numbers (must be integral or floating-point).
+     * @param nbr The number of random numbers to generate.
+     * @param max The maximum value (inclusive).
+     * @return A vector of random numbers of type `Type` in the range [0, max].
+     */
     template <typename Type>
     std::vector<Type> get_range(std::size_t nbr, const Type& max) {
       return get_range<Type>(nbr, static_cast<Type>(0), max);
     }
     
+    /**
+     * @brief Generates a N vector of random numbers in the range [0, 1].
+     *
+     * @tparam Type The type of the random numbers (must be integral or floating-point).
+     * @param nbr The number of random numbers to generate.
+     * @return A vector of random numbers of type `Type` in the range [0, 1].
+     */
     template <typename Type>
     std::vector<Type> get_range(std::size_t nbr) {
       return get_range<Type>(nbr, static_cast<Type>(0), static_cast<Type>(1));
     }
     
-    Engine get_engine() const {
+    /**
+     * @return The underlying random engine.
+     */
+    Engine& get_engine() {
       return gen_;
     }
 
@@ -91,8 +157,12 @@ namespace random {
     Engine gen_;
   };
   
-  inline Random<>& get_default_random()
-  {
+  /**
+   * @brief Returns a thread-local default random number generator instance.
+   *
+   * @return A reference to the thread-local default @ref Random instance.
+   */
+  inline Random<>& get_default_random() {
     static thread_local Random<> default_random;
 #ifdef DEBUG_TRACES
     std::clog << "get_default_random() " << &default_random << "\n";
