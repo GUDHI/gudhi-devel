8c347716200a0f7805b4565ce7f20960a40e8ae3
hschreiber <hannah.schreiber.k@googlemail.com>
2026-04-29 18:17:03 +0200

    generalization of vineyard plot options

--- src/python/gudhi/vineyard_graphical_tools.py
+++ src/python/gudhi/vineyard_graphical_tools.py
@@ -20,6 +20,12 @@ import matplotlib.pyplot as plt
 from gudhi.vineyard import Vineyard, PointCloudRipsVineyard
 
 
+def _setdefault_aliases(kwargs, default_value, *aliases):
+    """Set a default only if none of the aliases are already in kwargs."""
+    if not any(alias in kwargs for alias in aliases):
+        kwargs[aliases[0]] = default_value
+
+
 def _gray_on_band(ax, x, y, z, band):
     gray = (0.75, 0.75, 0.75)
     mask = np.concatenate(([0], np.asarray(y - x <= band), [0]))
@@ -27,7 +33,7 @@ def _gray_on_band(ax, x, y, z, band):
     starts = np.where(diff == 1)[0]
     ends = np.where(diff == -1)[0]
     for s, e in zip(starts, ends):
-        ax.plot3D(x[s:e], y[s:e], z[s:e], c=gray)
+        ax.plot(x[s:e], y[s:e], z[s:e], c=gray)
 
 
 def _erase_on_band(ax, x, y, z, band, c):
@@ -36,42 +42,64 @@ def _erase_on_band(ax, x, y, z, band, c):
     starts = np.where(diff == 1)[0]
     ends = np.where(diff == -1)[0]
     for s, e in zip(starts, ends):
-        ax.plot3D(x[s:e], y[s:e], z[s:e], c=c)
+        ax.plot(x[s:e], y[s:e], z[s:e], c=c)
 
 
-def _get_colors_for_all_dim(dim: int):
-    plot_colors = [
-        "Oranges",
-        "Purples",
-        "Greens",
-        "Blues",
-        "Reds",
-        "YlOrBr",
-        "PuRd",
-        "YlOrRd",
-        "OrRd",
-        "RdPu",
-        "BuPu",
-        "GnBu",
-        "PuBu",
-        "YlGnBu",
-        "PuBuGn",
-        "BuGn",
-        "YlGn",
-    ]
-    return mpl.colormaps[plot_colors[dim % len(plot_colors)]]
+def _get_default_colors(dim: int | None):
+    if dim is None:
+        return [
+            "Oranges",
+            "Purples",
+            "Greens",
+            "Blues",
+            "Reds",
+            "YlOrBr",
+            "PuRd",
+            "YlOrRd",
+            "OrRd",
+            "RdPu",
+            "BuPu",
+            "GnBu",
+            "PuBu",
+            "YlGnBu",
+            "PuBuGn",
+            "BuGn",
+            "YlGn",
+        ]
+    return ["viridis"]
 
 
-def _plot_vines(
-    ax,
-    vines,
-    inf_v,
-    min_bar_length,
-    noise_option: Literal[
-        "none", "gray_diagonal", "gray_band", "erase_diagonal", "erase_band"
-    ],
-    cmap,
-):
+def _get_vine_color_map(cmap, base, dim, nber_vines):
+    if cmap is None:
+        nber_colors = len(base)
+        if dim >= nber_colors:
+            dim = dim % nber_colors
+        cmap = mpl.colormaps[base[dim]]
+        return list(reversed(cmap(np.linspace(0, 1, nber_vines + 2))))
+
+    if cmap.ndim == 2:
+        if cmap.shape[1] != 4:
+            raise ValueError(
+                "Given cmap does not have the right shape. Expected shape is (*, 4) or (*, *, 4)."
+            )
+        return cmap
+
+    if cmap.ndim == 3:
+        if cmap.shape[2] != 4:
+            raise ValueError(
+                "Given cmap does not have the right shape. Expected shape is (*, 4) or (*, *, 4)."
+            )
+        nber_colors = cmap.shape[0]
+        if dim >= nber_colors:
+            dim = dim % nber_colors
+        return cmap[dim]
+
+    raise ValueError(
+        "Given cmap does not have the right shape. Expected shape is (*, 4) or (*, *, 4)."
+    )
+
+
+def _filter_vines(vines, inf_v, min_bar_length):
     real_vines = []
     for i in range(vines.shape[0]):
         x = vines[i, :, 0]
@@ -83,18 +111,28 @@ def _plot_vines(
         diff[diff < min_bar_length] = 0
         if np.any(diff):
             real_vines.append((x, y, z))
+    return real_vines
 
-    cmap = list(reversed(cmap(np.linspace(0, 1, len(real_vines) + 2))))
 
-    for i, (x, y, z) in enumerate(real_vines):
+def _plot_vines(
+    ax,
+    vines,
+    min_bar_length,
+    noise_option: Literal[
+        "none", "gray_diagonal", "gray_band", "erase_diagonal", "erase_band"
+    ],
+    cmap,
+):
+    i = 0
+    for x, y, z in vines:
         match noise_option:
             case "none":
-                ax.plot3D(x, y, z, c=cmap[i])
+                ax.plot(x, y, z, c=cmap[i])
             case "gray_diagonal":
-                ax.plot3D(x, y, z, c=cmap[i])
+                ax.plot(x, y, z, c=cmap[i])
                 _gray_on_band(ax, x, y, z, 0)
             case "gray_band":
-                ax.plot3D(x, y, z, c=cmap[i])
+                ax.plot(x, y, z, c=cmap[i])
                 _gray_on_band(ax, x, y, z, min_bar_length)
             case "erase_diagonal":
                 _erase_on_band(ax, x, y, z, 0, cmap[i])
@@ -106,12 +144,15 @@ def _plot_vines(
                     + noise_option
                     + ". Possibilities are: `none`, `gray_diagonal`, `gray_band`, `erase_diagonal` and `erase_band`"
                 )
+        i = i + 1
+        if i == len(cmap):
+            i = 0
 
 
 # TODO: option to handle points at infinity
 def plot_vineyards(
     vineyard: Vineyard | PointCloudRipsVineyard,
-    ax = None,
+    ax=None,
     dim: Optional[int] = None,
     max_dim: Optional[int] = None,
     min_bar_length: np.number = -1,
@@ -119,6 +160,7 @@ def plot_vineyards(
         "none", "gray_diagonal", "gray_band", "erase_diagonal", "erase_band"
     ] = "gray_diagonal",
     square_scaling: Optional[bool] = True,
+    cmap: Optional[np.ndarray] = None,
 ):
     """Plots the given vineyard, except for completely trivial vines (vines where all coordinates are on
     the diagonal). The points at infinity are mapped to a finite point a bit away from the other points.
@@ -153,7 +195,17 @@ def plot_vineyards(
     :param square_scaling: If `True`, the min and max values of the birth (x) and death (y) axis of the plot
         are equalized. Defaults to `True`. In any case, the scale ration of both axis will be equal.
     :type square_scaling: bool, optional
+    :param cmap: Optional. Numpy array of RGBA colors (ndim == 2) or numpy array of numpy arrays of RGBA colors
+        (ndim == 3) compatible with Matplotlib to color the vines. If an array of array is provided, a different array
+        is used for each different dimension.
+        If there are less colors than vines or ranges of colors than dimensions, a same color/range of colors will be
+        used several times.
+        They can for example be constructed from a `matplotlib.colors.Colormap \
+        <https://matplotlib.org/stable/api/_as_gen/matplotlib.colors.Colormap.html#matplotlib.colors.Colormap>`_.
+        Defaults to None.
+    :type cmap: Numpy array of shape (*, 4) or (*, *, 4), optional
     :raises ValueError: If the value provided for `noise_option` is not valid.
+    :raises ValueError: If cmap is provided and its shape is neither (*, 4) nor (*, *, 4).
     :return: Matplotlib axis into which was plotted.
     :rtype: `mpl_toolkits.mplot3d.axes3d.Axes3D \
         <https://matplotlib.org/stable/api/toolkits/mplot3d/axes3d.html#mpl_toolkits.mplot3d.axes3d.Axes3D>`_
@@ -161,6 +213,10 @@ def plot_vineyards(
     if ax is None:
         fig = plt.figure()
         ax = fig.add_subplot(projection="3d")
+    if cmap is None:
+        color_base = _get_default_colors(dim)
+    else:
+        color_base = None
 
     if dim is None:
         vy = vineyard.get_current_vineyard_view()
@@ -176,14 +232,17 @@ def plot_vineyards(
         inf_v = max_death + 10
 
         for d, vines in enumerate(vy):
-            cmap = _get_colors_for_all_dim(d)
-            _plot_vines(ax, vines, inf_v, min_bar_length, noise_option, cmap)
+            real_vines = _filter_vines(vines, inf_v, min_bar_length)
+            d_cmap = _get_vine_color_map(cmap, color_base, d, len(real_vines))
+            _plot_vines(ax, real_vines, min_bar_length, noise_option, d_cmap)
     else:
         vines = vineyard.get_current_vineyard_view(dim=dim)
         max_death = np.max(vines[:, :, 1], where=~np.isinf(vines[:, :, 1]), initial=-1)
         # arbitrary, just to distance them a bit from the finite bars
         inf_v = max_death + 10
-        _plot_vines(ax, vines, inf_v, min_bar_length, noise_option, mpl.colormaps["viridis"])
+        real_vines = _filter_vines(vines, inf_v, min_bar_length)
+        d_cmap = _get_vine_color_map(cmap, color_base, dim, len(real_vines))
+        _plot_vines(ax, real_vines, min_bar_length, noise_option, d_cmap)
 
     if square_scaling:
         scaling = np.array([getattr(ax, "get_{}lim".format(dim))() for dim in "xy"])
@@ -196,20 +255,26 @@ def plot_vineyards(
     return ax
 
 
-def _plot_cycle(axes, cycle, points, cpx, c):
+def _plot_cycle(axes, cycle, points, cpx, c, ls, lw):
     for u, v in [(points[cpx[idx][0]], points[cpx[idx][1]]) for idx in cycle]:
         if points.shape[1] == 2:
-            axes.plot([u[0], v[0]], [u[1], v[1]], color=c)
+            axes.plot([u[0], v[0]], [u[1], v[1]], color=c, linestyle=ls, linewidth=lw)
         else:
-            axes.plot([u[0], v[0]], [u[1], v[1]], [u[2], v[2]], color=c)
+            axes.plot(
+                [u[0], v[0]], [u[1], v[1]], [u[2], v[2]], color=c, linestyle=ls, linewidth=lw
+            )
 
 
 def plot_1D_representative_cycles(
     vineyard: Vineyard | PointCloudRipsVineyard,
     step: int,
-    ax = None,
+    ax=None,
     index: Optional[int] = None,
     min_bar_length: Optional[np.number] = None,
+    cmap: Optional[np.ndarray] = None,
+    edge_linestyle: Optional[str | tuple] = None,
+    edge_linewidth: Optional[float] = None,
+    **point_kwargs,
 ):
     """If `store_cycles` and `store_point_coordinates` were set to `True` at construction of `vineyard`, plots the
     representative 1-cycles at given step, except for those representing bars of length 0. The point coordinates must
@@ -227,9 +292,26 @@ def plot_1D_representative_cycles(
     :param min_bar_length: Optional. If provided, only plots 1-cycles corresponding to bars with length at least
         `min_bar_length`. Defaults to None (i.e., all non-trivial ones).
     :type min_bar_length: Any numerical type coercible to the filtration value type, optional
+    :param cmap: Optional. Array of RGBA colors compatible with Matplotlib to color the plotted cycles. It can for
+        example be constructed from a `matplotlib.colors.Colormap \
+        <https://matplotlib.org/stable/api/_as_gen/matplotlib.colors.Colormap.html#matplotlib.colors.Colormap>`_.
+        If there are less colors than cycles, a same color will be used several times. Defaults to None.
+    :type cmap: Numpy array of RGBA values, optional
+    :param edge_linestyle: Optional. Line style for the edges of the cycles. See `matplotlib.lines.Line2D \
+        <https://matplotlib.org/stable/api/_as_gen/matplotlib.lines.Line2D.html#matplotlib.lines.Line2D.set_linestyle>`_
+        for all possibilities. Defaults to None.
+    :type edge_linestyle: `str or tuple \
+        <https://matplotlib.org/stable/api/_as_gen/matplotlib.lines.Line2D.html#matplotlib.lines.Line2D.set_linestyle>`_
+        , optional
+    :param edge_linewidth: Optional. Line width in points for the edges of the cycles. Defaults to None.
+    :type edge_linewidth: float, optional
+    :param point_kwargs: Optional. All arguments to forward to the `matplotlib.axes.Axes.plot \
+        <https://matplotlib.org/stable/api/_as_gen/matplotlib.axes.Axes.plot.html#matplotlib.axes.Axes.plot>`_ method
+        when plotting the points.
     :raises NotImplementedError: If `store_cycles` or `store_point_coordinates` were set to `False` at construction.
     :raises ValueError: If the number of coordinates of a point is different from 2 or 3.
     :raises IndexError: If `index` is provided and is out of range.
+    :raises ValueError: If cmap is provided and its shape is not (*, 4).
     :return: Matplotlib axis into which was plotted.
     :rtype: `mpl_toolkits.mplot3d.axes3d.Axes3D \
         <https://matplotlib.org/stable/api/toolkits/mplot3d/axes3d.html#mpl_toolkits.mplot3d.axes3d.Axes3D>`_
@@ -247,40 +329,53 @@ def plot_1D_representative_cycles(
         else:
             raise ValueError("Plotting only possible in 2D and 3D.")
 
-    cmap_f = mpl.colormaps["tab10"]
-    cmap = cmap_f(np.arange(10))
-    cmap_f = mpl.colormaps["Set2"]
-    cmap = np.concatenate((cmap, cmap_f(np.arange(8))))
-    cmap_f = mpl.colormaps["Dark2"]
-    cmap = np.concatenate((cmap, cmap_f(np.arange(8))))
-    csize = 10 + 8 + 8
+    # plot edges
+    if cmap is None or cmap.shape[0] == 0:
+        cmap_f = mpl.colormaps["tab10"]
+        cmap = cmap_f(np.arange(10))
+        cmap_f = mpl.colormaps["Set2"]
+        cmap = np.concatenate((cmap, cmap_f(np.arange(8))))
+        cmap_f = mpl.colormaps["Dark2"]
+        cmap = np.concatenate((cmap, cmap_f(np.arange(8))))
+    elif cmap.ndim != 2 or cmap.shape[1] != 4:
+        raise ValueError("Given cmap does not have the right shape. Expected shape is (*, 4).")
+
     i = 0
 
     if index is None:
         if min_bar_length is None:
             for _, c in cycles.items():
-                _plot_cycle(ax, c, points, cpx, cmap[i])
-                i = (i + 1) % csize
+                _plot_cycle(ax, c, points, cpx, cmap[i], edge_linestyle, edge_linewidth)
+                i = i + 1
+                if i == cmap.shape[0]:
+                    i = 0
         else:
             vy = vineyard.get_current_vineyard_view(dim=1)
             for (_, idx), c in cycles.items():
                 if vy[idx][step][1] - vy[idx][step][0] >= min_bar_length:
-                    _plot_cycle(ax, c, points, cpx, cmap[i])
-                    i = (i + 1) % csize
+                    _plot_cycle(ax, c, points, cpx, cmap[i], edge_linestyle, edge_linewidth)
+                    i = i + 1
+                    if i == cmap.shape[0]:
+                        i = 0
     else:
         if min_bar_length is not None:
             warnings.warn(
                 "Specified argument `min_bar_length` is ignored.",
                 UserWarning,
             )
-        _plot_cycle(ax, cycles[index][0], points, cpx, cmap[i])
+        _plot_cycle(ax, cycles[index][0], points, cpx, cmap[i], edge_linestyle, edge_linewidth)
 
+    # plot points
     gray = (0.25, 0.25, 0.25)
     x = points[:, 0]
     y = points[:, 1]
+    _setdefault_aliases(point_kwargs, "none", "linestyle", "ls")
+    _setdefault_aliases(point_kwargs, 3, "markersize", "ms")
+    _setdefault_aliases(point_kwargs, gray, "color", "c")
+    point_kwargs.setdefault("marker", "o")
     if points.shape[1] == 2:
-        ax.plot(x, y, linestyle="none", markersize=3, marker="o", c=gray)
+        ax.plot(x, y, **point_kwargs)
     else:
-        ax.plot(x, y, points[:, 2], linestyle="none", markersize=3, marker="o", c=gray)
+        ax.plot(x, y, points[:, 2], **point_kwargs)
 
     return ax
