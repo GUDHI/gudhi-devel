13d9d9e5319785604693a0b284f4356f2d5b9c48
Vincent Rouvreau <vincent.rouvreau@inria.fr>
2026-06-05 13:57:43 +0200

    Skip these tests if tensorflow is not available. No python 3.14 tensorflow available for the moment

--- src/python/test/test_wasserstein_with_tensors.py
+++ src/python/test/test_wasserstein_with_tensors.py
@@ -5,13 +5,15 @@
     Copyright (C) 2020 Inria
 
     Modification(s):
+      - 2026/06 Vincent Rouvreau: Skip all tests if no tensorflow.
       - YYYY/MM Author: Description of the modification
 """
 
+import pytest
+tf = pytest.importorskip("tensorflow")  # All file is skipped if no tensorflow installed
 
 import numpy as np
 import torch
-import tensorflow as tf
 
 from gudhi.wasserstein import wasserstein_distance as pot
 
